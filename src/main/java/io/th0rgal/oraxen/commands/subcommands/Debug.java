175333921e87a74c9fa8f9b8351a09c5e210193e
Th0rgal <thomas.marchand44@gmail.com>
2019-09-22 18:30:06 +0200

    Fix: Durability replaced by durability

--- src/main/java/io/th0rgal/oraxen/commands/subcommands/Debug.java
+++ src/main/java/io/th0rgal/oraxen/commands/subcommands/Debug.java
@@ -3,9 +3,14 @@ package io.th0rgal.oraxen.commands.subcommands;
 import io.th0rgal.oraxen.commands.CommandInterface;
 import io.th0rgal.oraxen.items.Item;
 import io.th0rgal.oraxen.items.OraxenItems;
+import io.th0rgal.oraxen.utils.ItemUtils;
+import io.th0rgal.oraxen.utils.NMS;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class Debug implements CommandInterface {
 
@@ -14,7 +19,15 @@ public class Debug implements CommandInterface {
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.sendMessage("id:" + OraxenItems.getIdByItem(new Item(player.getInventory().getItemInMainHand())));
+            ItemStack item = player.getInventory().getItemInMainHand();
+            Object nmsItem = ItemUtils.getNMSCopy(item);
+            Object itemTag = ItemUtils.getNBTTagCompound(nmsItem);
+            Object nbtBase = ItemUtils.getNBTBase(itemTag, "Durability");
+            try {
+                player.sendMessage("durability:" + NMS.NBT_TAG_INT.toClass().getMethod("asInt").invoke(nbtBase));
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                e.printStackTrace();
+            }
         }
 
         return false;
