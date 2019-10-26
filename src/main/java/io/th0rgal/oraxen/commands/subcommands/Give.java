4163770b5917d011f43ee3028c49d6db7e21e6f1
Th0rgal <thomas.marchand44@gmail.com>
2019-10-26 11:31:51 +0200

    improves: give commands

--- src/main/java/io/th0rgal/oraxen/commands/subcommands/Give.java
+++ src/main/java/io/th0rgal/oraxen/commands/subcommands/Give.java
@@ -3,9 +3,11 @@ package io.th0rgal.oraxen.commands.subcommands;
 import io.th0rgal.oraxen.commands.CommandInterface;
 import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.settings.Message;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Give implements CommandInterface {
 
@@ -16,16 +18,23 @@ public class Give implements CommandInterface {
             Message.DONT_HAVE_PERMISSION.send(sender, "oraxen.command.give");
             return false;
         }
-        if (args.length != 2) return false;
 
-        if (sender instanceof Player) {
+        if (args.length < 2)
+            return false;
 
-            Player player = (Player)sender;
-            player.getInventory().addItem(OraxenItems.getItemById(args[1]).build());
+        else if (args.length == 2)
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.getInventory().addItem(OraxenItems.getItemById(args[1]).build());
+            } else
+                Message.NOT_A_PLAYER_ERROR.send(sender);
 
-
-        } else {
-            Message.NOT_A_PLAYER_ERROR.send(sender);
+        else {
+            Player target = Bukkit.getPlayer(args[1]);
+            ItemStack itemStack = OraxenItems.getItemById(args[2]).build();
+            if (args.length == 4)
+                itemStack.setAmount(Integer.parseInt(args[3]));
+            target.getInventory().addItem(itemStack);
         }
 
         return true;
