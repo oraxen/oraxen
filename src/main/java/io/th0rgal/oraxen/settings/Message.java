0cc9129ac86d67f1d75678907a33e857a837cc03
Th0rgal <thomas.marchand44@gmail.com>
2019-11-01 22:35:03 +0100

    ♻️ refactors Messages Enum

--- src/main/java/io/th0rgal/oraxen/settings/Message.java
+++ src/main/java/io/th0rgal/oraxen/settings/Message.java
@@ -7,14 +7,20 @@ import org.bukkit.command.CommandSender;
 
 public enum Message {
 
-    NOT_A_PLAYER_ERROR(ChatColor.RED.toString() + "You must be a player to use this command!"),
-    COMMAND_DOES_NOT_EXIST_ERROR(ChatColor.RED.toString() + "This command doesn't exist, check the doc!"),
-    ZIP_BROWSE_ERROR(ChatColor.RED.toString() + "An error occured browsing the zip"),
-    DONT_HAVE_PERMISSION(ChatColor.RED.toString() + "You need the permission %s to do this"),
-    MECHANIC_DOESNT_EXIST(ChatColor.RED.toString() + "The mechanic %s doesn't exist"),
-    WRONG_TYPE(ChatColor.RED.toString() + "You are using an invalid type");
+    CMD_HELP(ChatColor.GREEN.toString() + ""),
+
+    NOT_A_PLAYER_ERROR(ChatColor.RED, "You must be a player to use this command!"),
+    COMMAND_DOES_NOT_EXIST_ERROR(ChatColor.RED, "This command doesn't exist, check the doc!"),
+    ZIP_BROWSE_ERROR(ChatColor.RED, "An error occured browsing the zip"),
+    DONT_HAVE_PERMISSION(ChatColor.RED, "You need the permission %s to do this"),
+    MECHANIC_DOESNT_EXIST(ChatColor.RED, "The mechanic %s doesn't exist"),
+    WRONG_TYPE(ChatColor.RED, "You are using an invalid type");
 
     private String message;
+
+    Message(ChatColor color, String message) {
+        this.message = color + message;
+    }
 
     Message(String message) {
         this.message = message;
