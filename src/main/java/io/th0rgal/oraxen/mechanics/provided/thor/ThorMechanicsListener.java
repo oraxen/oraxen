80cb13f00ba9dfb34088bef012e1dacefd3121af
Th0rgal <thomas.marchand44@gmail.com>
2019-11-09 20:06:58 +0100

    :sparkles: introduces lightning strikes

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/thor/ThorMechanicsListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/thor/ThorMechanicsListener.java
@@ -1,7 +1,14 @@
 package io.th0rgal.oraxen.mechanics.provided.thor;
 
+import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class ThorMechanicsListener implements Listener {
 
@@ -9,6 +16,21 @@ public class ThorMechanicsListener implements Listener {
 
     public ThorMechanicsListener(MechanicFactory factory) {
         this.factory = factory;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onCall(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK)
+            return;
+
+        ItemStack item = event.getItem();
+        String itemID = OraxenItems.getIdByItem(item);
+
+        if (factory.isNotImplementedIn(itemID))
+            return;
+
+        Player player = event.getPlayer();
+        player.getWorld().strikeLightning(event.getPlayer().getTargetBlock(null, 50).getLocation());
     }
 
 }
