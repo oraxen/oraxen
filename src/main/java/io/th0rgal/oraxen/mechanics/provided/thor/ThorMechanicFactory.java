9bff3f55ab0d85b24a2a9ef5b1ff860cc626de38
Th0rgal <thomas.marchand44@gmail.com>
2019-11-09 19:31:57 +0100

    :construction: creating initial files for thor mechanic

---
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/thor/ThorMechanicFactory.java
@@ -0,0 +1,4 @@
+package io.th0rgal.oraxen.mechanics.provided.thor;
+
+public class ThorMechanicFactory {
+}
---
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/thor/ThorMechanicsListener.java
@@ -0,0 +1,6 @@
+package io.th0rgal.oraxen.mechanics.provided.thor;
+
+import org.bukkit.event.Listener;
+
+public class ThorMechanicsListener implements Listener {
+}
