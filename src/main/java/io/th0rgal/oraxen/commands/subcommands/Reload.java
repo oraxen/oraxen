bd6108b1f7487fce319116f9d2159d87a462709b
Th0rgal <thomas.marchand44@gmail.com>
2019-11-10 18:05:18 +0100

    :sparkles: introduces reload command

--- src/main/java/io/th0rgal/oraxen/commands/subcommands/Reload.java
+++ src/main/java/io/th0rgal/oraxen/commands/subcommands/Reload.java
@@ -1,10 +1,16 @@
 package io.th0rgal.oraxen.commands.subcommands;
 
+import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.commands.CommandInterface;
+import io.th0rgal.oraxen.items.OraxenItems;
+import io.th0rgal.oraxen.pack.generation.ResourcePack;
+import io.th0rgal.oraxen.pack.upload.UploadManager;
 import io.th0rgal.oraxen.settings.Message;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class Reload implements CommandInterface {
 
@@ -15,10 +21,22 @@ public class Reload implements CommandInterface {
             Message.DONT_HAVE_PERMISSION.send(sender, "oraxen.command.reload");
             return false;
         }
+        JavaPlugin plugin = OraxenPlugin.get();
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
+        switch (args[1]) {
+            case "items":
+                Message.RELOAD.send(sender, "items");
+                OraxenItems.loadItems(plugin);
+                break;
 
+            case "pack":
+                Message.REGENERATED.send(sender, "resourcepack");
+                File resourcePack = ResourcePack.generate(plugin);
+                new UploadManager(plugin).uploadAsyncAndSendToPlayers(resourcePack);
+                break;
+
+            default:
+                break;
         }
 
         return false;
