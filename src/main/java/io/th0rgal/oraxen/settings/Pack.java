8c9d2ae1ea99e9e5c369da7774465ea47fae214b
Th0rgal <thomas.marchand44@gmail.com>
2019-11-11 19:25:35 +0100

    :sparkles: updates to new configuration

--- src/main/java/io/th0rgal/oraxen/settings/Pack.java
+++ src/main/java/io/th0rgal/oraxen/settings/Pack.java
@@ -4,11 +4,15 @@ import io.th0rgal.oraxen.OraxenPlugin;
 import org.bukkit.ChatColor;
 
 public enum Pack implements ConfigEnum {
-    GENERATE("generate"),
-    UPLOAD("upload"),
-    SEND("send"),
-    COMPRESSION("compression"),
-    COMMENT("comment");
+    GENERATE("generation.generate"),
+    COMPRESSION("generation.compression"),
+    COMMENT("generation.comment"),
+
+    UPLOAD("upload.enabled"),
+
+    SEND_PACK("dispatch.send_pack"),
+    SEND_PACK_MENU("dispatch.send_pack_menu"),
+    MENU_JSON("dispatch.menu_json");
 
     private final String section;
     private static final ResourcesManager RESOURCES_MANAGER = new ResourcesManager(OraxenPlugin.get());
