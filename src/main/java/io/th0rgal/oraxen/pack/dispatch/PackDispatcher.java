ad3171fbc37b1924184c26f19f03943794edb4b1
Th0rgal <thomas.marchand44@gmail.com>
2019-11-11 21:20:43 +0100

    :sparkles: adds pack menu inside a book

--- src/main/java/io/th0rgal/oraxen/pack/dispatch/PackDispatcher.java
+++ src/main/java/io/th0rgal/oraxen/pack/dispatch/PackDispatcher.java
@@ -1,14 +1,13 @@
 package io.th0rgal.oraxen.pack.dispatch;
 
-import io.th0rgal.oraxen.settings.Pack;
 import io.th0rgal.oraxen.utils.NMS;
+import net.md_5.bungee.api.chat.*;
 import net.minecraft.server.v1_14_R1.EnumHand;
 import net.minecraft.server.v1_14_R1.PacketPlayOutOpenBook;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
-import net.md_5.bungee.chat.ComponentSerializer;
 
 public class PackDispatcher {
 
@@ -25,7 +24,20 @@ public class PackDispatcher {
     public static void sendMenu(Player player) {
         ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
         BookMeta meta = (BookMeta) book.getItemMeta();
-        meta.spigot().setPages(ComponentSerializer.parse(Pack.MENU_JSON.toString()));
+
+        BaseComponent page = new TextComponent("§8§lRESOURCE PACK\nTo see the new items you need to use a resourcepack\n\nTo try to load it directly from the game, ");
+        BaseComponent clic = new TextComponent("§a§l§nCLICK HERE");
+        clic.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Clique here to download the resourcepack from the website").create()));
+        clic.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+        page.addExtra(clic);
+        page.addExtra("\nTo download it from the internet, ");
+        BaseComponent cmd = new TextComponent("§r§c§l§nCLICK HERE");
+        cmd.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to load directly the resourcepack from the game").create()));
+        cmd.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/oraxen pack getpack"));
+        page.addExtra(cmd);
+
+        meta.spigot().addPage(new BaseComponent[]{page});
+        book.setItemMeta(meta);
 
         ItemStack held = player.getInventory().getItemInMainHand();
         player.getInventory().setItemInMainHand(book);
