e685df0de841c25e298eb20e5c59ce2d81b2783b
Th0rgal <thomas.marchand@tuta.io>
2019-11-30 11:09:54 +0100

    :truck: renames file

--- src/main/java/io/th0rgal/oraxen/recipes/listeners/PermissionRecipesManager.java
+++ src/main/java/io/th0rgal/oraxen/recipes/listeners/PermissionRecipesManager.java
@@ -5,11 +5,17 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.inventory.Recipe;
 
-public class RecipesEvents implements Listener {
+import java.util.Map;
+
+public class PermissionRecipesManager implements Listener {
+
+    private static Map<Recipe, String> permissionsPerRecipe;
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
     public void onCrafted(PrepareItemCraftEvent event) {
+        event.getRecipe();
 
     }
 
