353b6c597297f9464988da0643cbe1ba1079da3c
Th0rgal <thomas.marchand@tuta.io>
2019-11-30 11:04:13 +0100

    :construction: adds new recipes events

---
+++ src/main/java/io/th0rgal/oraxen/recipes/listeners/RecipesEvents.java
@@ -0,0 +1,16 @@
+package io.th0rgal.oraxen.recipes.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+
+public class RecipesEvents implements Listener {
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
+    public void onItemDamaged(PrepareItemCraftEvent event) {
+
+    }
+
+}
