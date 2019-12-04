543cb65c3049e4e0cfe41fa2e8020859bbc773d8
Th0rgal <thomas.marchand44@gmail.com>
2019-12-04 14:08:30 +0100

    :sparkles: adds Listener

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanicFactory.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanicFactory.java
@@ -1,14 +1,17 @@
 package io.th0rgal.oraxen.mechanics.provided.bottledexp;
 
+import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.mechanics.Mechanic;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
-import io.th0rgal.oraxen.mechanics.provided.durability.DurabilityMechanic;
+import io.th0rgal.oraxen.mechanics.MechanicsManager;
 import org.bukkit.configuration.ConfigurationSection;
 
 public class BottledExpMechanicFactory extends MechanicFactory {
 
     public BottledExpMechanicFactory(ConfigurationSection section) {
         super(section);
+        MechanicsManager.registerListeners(OraxenPlugin.get(),
+                new BottledExpMechanicListener(this));
     }
 
     @Override
