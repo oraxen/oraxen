de5ef3323c85b800ad27d174d8906563e799d5dc
Th0rgal <thomas.marchand44@gmail.com>
2019-12-04 14:25:52 +0100

    :sparkles: adds ratio

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanic.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanic.java
@@ -6,8 +6,15 @@ import org.bukkit.configuration.ConfigurationSection;
 
 public class BottledExpMechanic extends Mechanic {
 
+    final double ratio;
+
     public BottledExpMechanic(MechanicFactory mechanicFactory, ConfigurationSection section) {
         super(mechanicFactory, section);
+        this.ratio = section.getDouble("ratio");
+    }
+
+    public double getRatio() {
+        return  ratio;
     }
 
 }
