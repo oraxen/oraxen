42bf308c090b46ff1a61e66655692ef920b373f3
Th0rgal <thomas.marchand44@gmail.com>
2019-12-04 14:00:55 +0100

    :tada: BottledExpMechanic initial commit

---
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanic.java
@@ -0,0 +1,13 @@
+package io.th0rgal.oraxen.mechanics.provided.bottledexp;
+
+import io.th0rgal.oraxen.mechanics.Mechanic;
+import io.th0rgal.oraxen.mechanics.MechanicFactory;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class BottledExpMechanic extends Mechanic {
+
+    public BottledExpMechanic(MechanicFactory mechanicFactory, ConfigurationSection section) {
+        super(mechanicFactory, section);
+    }
+
+}
