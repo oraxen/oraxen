8b82ab72fe63fbcc690c3db961a32a5ab02b8231
Th0rgal <thomas.marchand44@gmail.com>
2019-12-04 14:15:03 +0100

    :sparkles: detects implemented items

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanicListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/bottledexp/BottledExpMechanicListener.java
@@ -1,7 +1,12 @@
 package io.th0rgal.oraxen.mechanics.provided.bottledexp;
 
+import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
+
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class BottledExpMechanicListener implements Listener {
 
@@ -9,6 +14,25 @@ public class BottledExpMechanicListener implements Listener {
 
     public BottledExpMechanicListener(BottledExpMechanicFactory factory) {
         this.factory = factory;
+    }
+
+    public void onRightClick(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        if (action != Action.LEFT_CLICK_AIR
+                && action != Action.RIGHT_CLICK_AIR
+                && action != Action.LEFT_CLICK_BLOCK
+                && action != Action.RIGHT_CLICK_BLOCK)
+            return;
+
+        ItemStack item = event.getItem();
+        if (item == null)
+            return;
+
+        String itemID = OraxenItems.getIdByItem(item);
+        if (factory.isNotImplementedIn(itemID))
+            return;
+
+        BottledExpMechanic mechanic = (BottledExpMechanic) factory.getMechanic(itemID);
     }
 
 }
