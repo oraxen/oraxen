fc35ad7811137985a8ef9ce00ca36d25886182c6
Th0rgal <thomas.marchand@tuta.io>
2019-12-25 21:28:11 +0100

    :sparkles: adds depth option

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/bigmining/BigMiningMechanic.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/bigmining/BigMiningMechanic.java
@@ -7,14 +7,20 @@ import org.bukkit.configuration.ConfigurationSection;
 public class BigMiningMechanic extends Mechanic {
 
     private int radius;
+    private int depth;
 
     public BigMiningMechanic(MechanicFactory mechanicFactory, ConfigurationSection section) {
         super(mechanicFactory, section);
         radius = section.getInt("radius");
+        depth = section.getInt("depth");
     }
 
     public int getRadius() {
         return this.radius;
+    }
+
+    public int getDepth() {
+        return this.depth;
     }
 
 }
--- src/main/java/io/th0rgal/oraxen/mechanics/provided/bigmining/BigMiningMechanicsListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/bigmining/BigMiningMechanicsListener.java
@@ -3,6 +3,7 @@ package io.th0rgal.oraxen.mechanics.provided.bigmining;
 import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
 
+import io.th0rgal.oraxen.utils.logs.Logs;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -13,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import java.util.List;
 
@@ -41,36 +43,49 @@ public class BigMiningMechanicsListener implements Listener {
         if (factory.isNotImplementedIn(itemID))
             return;
 
-        BigMiningMechanic mechanic = (BigMiningMechanic)factory.getMechanic(itemID);
+        BigMiningMechanic mechanic = (BigMiningMechanic) factory.getMechanic(itemID);
 
         Player player = event.getPlayer();
         List<Block> lastTwoTargetBlocks = player.getLastTwoTargetBlocks(null, 5);
-        BlockFace blockFace = lastTwoTargetBlocks.get(1).getFace(lastTwoTargetBlocks.get(0));
+        Block firstBlock = lastTwoTargetBlocks.get(0);
+        Block secondBlock = lastTwoTargetBlocks.get(1);
+        BlockFace blockFace = secondBlock.getFace(firstBlock);
 
-        for (double relativeX = -mechanic.getRadius(); relativeX <= mechanic.getRadius(); relativeX++)
-            for (double relativeY = -mechanic.getRadius(); relativeY <= mechanic.getRadius(); relativeY++) {
-                if (relativeX == 0 && relativeY == 0)
-                    continue;
-                Location location = event.getBlock().getLocation();
-                transpose(location, blockFace, relativeX, relativeY);
-                blocksToProcess += 1; // to avoir this method to call itself
-                Block block = location.getBlock();
-                BlockBreakEvent blockBreakEvent = new BlockBreakEvent(block, player);
-                Bukkit.getPluginManager().callEvent(blockBreakEvent);
-                if (!blockBreakEvent.isCancelled())
-                    block.breakNaturally();
-            }
+        int iterations = mechanic.getDepth();
+
+        Block initialBlock = event.getBlock();
+        Vector locationModifier = firstBlock.getLocation().toVector().subtract(secondBlock.getLocation().toVector());
+        Location processedLocation = initialBlock.getLocation().clone();
+        while (iterations >= 1) {
+            for (double relativeX = -mechanic.getRadius(); relativeX <= mechanic.getRadius(); relativeX++)
+                for (double relativeY = -mechanic.getRadius(); relativeY <= mechanic.getRadius(); relativeY++) {
+                    Location tempLocation = transpose(processedLocation, blockFace, relativeX, relativeY);
+                    if (tempLocation.equals(initialBlock.getLocation()))
+                        continue;
+
+                    Block block = tempLocation.getBlock();
+                    blocksToProcess += 1; // to avoir this method to call itself
+                    BlockBreakEvent blockBreakEvent = new BlockBreakEvent(block, player);
+                    Bukkit.getPluginManager().callEvent(blockBreakEvent);
+                    if (!blockBreakEvent.isCancelled())
+                        block.breakNaturally();
+                }
+            iterations -= 1;
+            processedLocation = processedLocation.subtract(locationModifier);
+        }
     }
 
     /*
         It converts a relative location in 2d into another location in 3d on a certain axis
      */
-    private void transpose(Location location, BlockFace blockFace, double relative_x, double relative_y) {
+    private Location transpose(Location location, BlockFace blockFace, double relative_x, double relative_y) {
+        location = location.clone();
         if (blockFace == BlockFace.WEST || blockFace == BlockFace.EAST)  // WEST_EAST axis != X
             location.add(0, relative_x, relative_y);
         else if (blockFace == BlockFace.DOWN || blockFace == BlockFace.UP)  // DOWN_UP axis != Y
             location.add(relative_x, 0, relative_y);
         else  // NORTH_SOUTH axis != Z
             location.add(relative_x, relative_y, 0);
+        return location;
     }
 }
