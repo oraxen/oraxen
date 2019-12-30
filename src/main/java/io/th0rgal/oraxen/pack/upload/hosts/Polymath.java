a650e9b1f76f0067e0838c62d682e6fd073b5de1
Thomas Marchand <thomas.marchand@tuta.io>
2019-12-30 12:24:52 +0100

    :fire: removes useless import

--- src/main/java/io/th0rgal/oraxen/pack/upload/hosts/Polymath.java
+++ src/main/java/io/th0rgal/oraxen/pack/upload/hosts/Polymath.java
@@ -3,7 +3,6 @@ package io.th0rgal.oraxen.pack.upload.hosts;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.roxstudio.utils.CUrl;
-import io.th0rgal.oraxen.utils.logs.Logs;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
