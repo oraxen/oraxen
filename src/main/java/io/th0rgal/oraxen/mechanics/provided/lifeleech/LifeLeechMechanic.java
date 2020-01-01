28cb0c185c0d3e2b971b3ec7e34c39fb00ce4f4b
Thomas Marchand <thomas.marchand@tuta.io>
2020-01-01 10:37:36 +0100

    :tada: initial commit

---
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/lifeleech/LifeLeechMechanic.java
@@ -0,0 +1,13 @@
+package io.th0rgal.oraxen.mechanics.provided.lifeleech;
+
+import io.th0rgal.oraxen.mechanics.Mechanic;
+import io.th0rgal.oraxen.mechanics.MechanicFactory;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class LifeLeechMechanic extends Mechanic {
+
+    public LifeLeechMechanic(MechanicFactory mechanicFactory, ConfigurationSection section) {
+        super(mechanicFactory, section);
+    }
+
+}
