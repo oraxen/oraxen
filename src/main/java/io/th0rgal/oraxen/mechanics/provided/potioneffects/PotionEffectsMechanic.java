12e980ef13718c02ec7107d2dd16a439134bf0cd
Thomas Marchand <thomas.marchand@tuta.io>
2020-02-08 22:02:07 +0100

    :fire: removes item potion binding feature (this is better for armor only)

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/potioneffects/PotionEffectsMechanic.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/potioneffects/PotionEffectsMechanic.java
@@ -12,14 +12,9 @@ import java.util.*;
 
 public class PotionEffectsMechanic extends Mechanic {
 
-    private Map<Position, Set<PotionEffect>> effects = new HashMap<>();
-    private Map<Position, Set<PotionEffectType>> overridedTypes = new HashMap<>();
+    private Set<PotionEffect> effects = new HashSet<>();
+    private Set<PotionEffectType> overridedTypes = new HashSet<>();
     private Map<UUID, Set<PotionEffect>> previousPlayerEffects = new HashMap<>();
-
-    enum Position {
-        HELD,
-        WORN
-    }
 
     public PotionEffectsMechanic(MechanicFactory mechanicFactory, ConfigurationSection section) {
         super(mechanicFactory, section);
@@ -43,47 +38,32 @@ public class PotionEffectsMechanic extends Mechanic {
         if (section.isBoolean("icon"))
             icon = section.getBoolean("icon");
         PotionEffect potionEffect = new PotionEffect(effectType, Integer.MAX_VALUE, amplifier, ambient, particles, icon);
-        Position position = Position.valueOf(section.getString("position").toUpperCase());
-
-        Set<PotionEffect> effectsPerPosition = effects.getOrDefault(position, new HashSet<>());
-        effectsPerPosition.add(potionEffect);
-        effects.put(position, effectsPerPosition);
-
-        Set<PotionEffectType> overridedTypesPerPosition = overridedTypes.getOrDefault(position, new HashSet<>());
-        overridedTypesPerPosition.add(potionEffect.getType());
-        overridedTypes.put(position, overridedTypesPerPosition);
+        effects.add(potionEffect);
+        overridedTypes.add(potionEffect.getType());
     }
 
-    public void onItemPlaced(Position position, Player player) {
-        if (!effects.containsKey(position))
-            return;
+    public void onItemPlaced(Player player) {
         Collection<PotionEffect> activeEffects = player.getActivePotionEffects();
         Set<PotionEffect> currentConflictingEffects = new HashSet<>();
         for (PotionEffect potionEffect : activeEffects)
             // to avoid player lose their previous effects or can usebug
-            if (potionEffect.getDuration() < 1728000 && !hasConflict(position, potionEffect))
+            if (potionEffect.getDuration() < 1728000 && !overridedTypes.contains(potionEffect.getType()))
                 currentConflictingEffects.add(potionEffect);
 
         if (!currentConflictingEffects.isEmpty())
             previousPlayerEffects.put(player.getUniqueId(), currentConflictingEffects);
 
-        player.addPotionEffects(effects.get(position));
+        player.addPotionEffects(effects);
     }
 
-    public void onItemRemoved(Position position, Player player) {
-        if (!overridedTypes.containsKey(position))
-            return;
-        for (PotionEffectType potionEffectType : overridedTypes.get(position))
+    public void onItemRemoved(Player player) {
+        for (PotionEffectType potionEffectType : overridedTypes)
             player.removePotionEffect(potionEffectType);
 
         if (previousPlayerEffects.containsKey(player.getUniqueId())) {
             player.addPotionEffects(previousPlayerEffects.get(player.getUniqueId()));
             previousPlayerEffects.remove(player.getUniqueId());
         }
-    }
-
-    private boolean hasConflict(Position position, PotionEffect potionEffect) {
-        return overridedTypes.get(position).contains(potionEffect.getType());
     }
 
 }
--- src/main/java/io/th0rgal/oraxen/mechanics/provided/potioneffects/PotionEffectsMechanicListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/potioneffects/PotionEffectsMechanicListener.java
@@ -3,13 +3,10 @@ package io.th0rgal.oraxen.mechanics.provided.potioneffects;
 import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.utils.armorequipevent.ArmorEquipEvent;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemStack;
-
 
 public class PotionEffectsMechanicListener implements Listener {
 
@@ -20,38 +17,17 @@ public class PotionEffectsMechanicListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onItemWorn(PlayerItemHeldEvent event) {
-        Player player = event.getPlayer();
-        ItemStack item = player.getInventory().getItem(event.getNewSlot());
-
-        String itemID = OraxenItems.getIdByItem(item);
-        if (!factory.isNotImplementedIn(itemID)) {
-            PotionEffectsMechanic mechanic = (PotionEffectsMechanic) factory.getMechanic(itemID);
-            mechanic.onItemPlaced(PotionEffectsMechanic.Position.HELD, event.getPlayer());
-            return;
-        }
-        item = player.getInventory().getItem(event.getPreviousSlot());
-        itemID = OraxenItems.getIdByItem(item);
-        if (!factory.isNotImplementedIn(itemID)) {
-            PotionEffectsMechanic mechanic = (PotionEffectsMechanic) factory.getMechanic(itemID);
-            mechanic.onItemRemoved(PotionEffectsMechanic.Position.HELD, event.getPlayer());
-        }
-    }
-
-    @EventHandler(priority = EventPriority.NORMAL)
     public void onItemWorn(ArmorEquipEvent event) {
         ItemStack item = event.getNewArmorPiece();
         String itemID = OraxenItems.getIdByItem(item);
         if (!factory.isNotImplementedIn(itemID)) {
             PotionEffectsMechanic mechanic = (PotionEffectsMechanic) factory.getMechanic(itemID);
-            mechanic.onItemPlaced(PotionEffectsMechanic.Position.WORN, event.getPlayer());
+            mechanic.onItemPlaced(event.getPlayer());
             return;
         }
-        item = event.getOldArmorPiece();
-        itemID = OraxenItems.getIdByItem(item);
         if (!factory.isNotImplementedIn(itemID)) {
             PotionEffectsMechanic mechanic = (PotionEffectsMechanic) factory.getMechanic(itemID);
-            mechanic.onItemRemoved(PotionEffectsMechanic.Position.WORN, event.getPlayer());
+            mechanic.onItemRemoved(event.getPlayer());
         }
     }
 
