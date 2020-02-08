c491ae752651b83986fda9d6fb6049705aaff665
Thomas Marchand <thomas.marchand@tuta.io>
2020-02-08 21:40:40 +0100

    :sparkles: adds check for held item

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/potioneffects/PotionEffectsMechanicListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/potioneffects/PotionEffectsMechanicListener.java
@@ -3,9 +3,11 @@ package io.th0rgal.oraxen.mechanics.provided.potioneffects;
 import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.utils.armorequipevent.ArmorEquipEvent;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 import org.bukkit.inventory.ItemStack;
 
 
@@ -15,6 +17,25 @@ public class PotionEffectsMechanicListener implements Listener {
 
     public PotionEffectsMechanicListener(PotionEffectsMechanicFactory factory) {
         this.factory = factory;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onItemWorn(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItem(event.getNewSlot());
+
+        String itemID = OraxenItems.getIdByItem(item);
+        if (!factory.isNotImplementedIn(itemID)) {
+            PotionEffectsMechanic mechanic = (PotionEffectsMechanic) factory.getMechanic(itemID);
+            mechanic.onItemPlaced(PotionEffectsMechanic.Position.HELD, event.getPlayer());
+            return;
+        }
+        item = player.getInventory().getItem(event.getPreviousSlot());
+        itemID = OraxenItems.getIdByItem(item);
+        if (!factory.isNotImplementedIn(itemID)) {
+            PotionEffectsMechanic mechanic = (PotionEffectsMechanic) factory.getMechanic(itemID);
+            mechanic.onItemRemoved(PotionEffectsMechanic.Position.HELD, event.getPlayer());
+        }
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
