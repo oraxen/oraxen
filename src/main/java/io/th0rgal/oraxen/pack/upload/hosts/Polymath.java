758b83ca4581520e775ec9fdbcf78be771fb0724
Thomas Marchand <thomas.marchand@tuta.io>
2020-02-09 21:26:41 +0100

    :sparkles: adds error handling

--- src/main/java/io/th0rgal/oraxen/pack/upload/hosts/Polymath.java
+++ src/main/java/io/th0rgal/oraxen/pack/upload/hosts/Polymath.java
@@ -3,6 +3,7 @@ package io.th0rgal.oraxen.pack.upload.hosts;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.roxstudio.utils.CUrl;
+import io.th0rgal.oraxen.utils.logs.Logs;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -24,6 +25,10 @@ public class Polymath implements HostingProvider {
                     .form("id", "%%__USER__%%")
                     .form("pack", new CUrl.FileIO(resourcePack.getPath()));
             JsonObject jsonOutput = (JsonObject) new JsonParser().parse(new String(curl.exec(), StandardCharsets.UTF_8));
+            if (jsonOutput.has("error")) {
+                Logs.logError(jsonOutput.get("error").getAsString());
+                throw new RuntimeException();
+            }
             packUrl = jsonOutput.get("url").getAsString();
             sha1 = jsonOutput.get("sha1").getAsString();
             return true;
@@ -45,7 +50,7 @@ public class Polymath implements HostingProvider {
         byte[] data = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
             data[i / 2] = (byte) ((Character.digit(sha1.charAt(i), 16) << 4)
-                    + Character.digit(sha1.charAt(i+1), 16));
+                    + Character.digit(sha1.charAt(i + 1), 16));
         }
         return data;
     }
