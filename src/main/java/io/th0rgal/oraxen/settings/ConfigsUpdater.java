d07d763bba85b2f74c37b3dab431369e6f373682
Thomas Marchand <thomas.marchand@tuta.io>
2020-02-10 21:48:09 +0100

    :tada: initial commit

--- src/main/java/io/th0rgal/oraxen/settings/ConfigsUpdater.java
+++ src/main/java/io/th0rgal/oraxen/settings/ConfigsUpdater.java
@@ -1,4 +1,15 @@
 package io.th0rgal.oraxen.settings;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class ConfigsUpdater {
+
+    JavaPlugin plugin;
+    public ConfigsUpdater (JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    
+
+
 }
