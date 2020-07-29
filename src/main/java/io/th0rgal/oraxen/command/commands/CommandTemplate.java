9fc5dfb477c782b4e12a152042f76aafe7dc535b
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-07-29 14:03:40 +0200

    Added template

---
+++ src/main/java/io/th0rgal/oraxen/command/commands/CommandTemplate.java
@@ -0,0 +1,19 @@
+package io.th0rgal.oraxen.command.commands;
+
+import org.bukkit.command.CommandSender;
+
+import com.oraxen.chimerate.commons.command.tree.nodes.Literal;
+import com.oraxen.chimerate.commons.command.tree.nodes.Literal.Builder;
+
+import io.th0rgal.oraxen.command.CommandInfo;
+
+public class CommandTemplate {
+
+    public static CommandInfo build() {
+        return new CommandInfo("name", info -> {
+            Builder<CommandSender> builder = Literal.of(info.getName()).alias(info.getAliases());
+            return builder;
+        }, "alias1", "alias2", "...");
+    }
+
+}
