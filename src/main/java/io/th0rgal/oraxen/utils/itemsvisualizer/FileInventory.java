c7dfca174a09fd1b3c88d031c75ee3583c477067
Bierque Jason <bierquejason@gmail.com>
2020-08-07 13:31:30 +0200

    Meilleur GUI du /o inv

--- src/main/java/io/th0rgal/oraxen/utils/itemsvisualizer/FileInventory.java
+++ src/main/java/io/th0rgal/oraxen/utils/itemsvisualizer/FileInventory.java
@@ -1,11 +1,15 @@
 package io.th0rgal.oraxen.utils.itemsvisualizer;
 
 import com.google.common.collect.Iterables;
+import org.bukkit.craftbukkit.libs.org.apache.commons.io.FilenameUtils;
+import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.items.ItemBuilder;
 import io.th0rgal.oraxen.items.OraxenItems;
+import io.th0rgal.oraxen.settings.ResourcesManager;
 import io.th0rgal.oraxen.utils.fastinv.FastInv;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -16,6 +20,8 @@ public class FileInventory extends FastInv {
     public FileInventory(int page) {
         super(3 * 9, "Oraxen item files visualizer");
 
+        YamlConfiguration settings = new ResourcesManager(OraxenPlugin.get()).getSettings();
+
         boolean lastPage = false;
         Collection<File> collection = OraxenItems.getMap().keySet();
         for (int i = page * 2 * 9; i < (page + 1) * 2 * 9; i++) {
@@ -24,8 +30,25 @@ public class FileInventory extends FastInv {
                 break;
             }
             File ymlFile = Iterables.get(collection, i);
-            ItemStack item = new ItemBuilder(Material.PAPER).setDisplayName(ChatColor.GREEN + ymlFile.getName()).build();
-            setItem(i - page * 2 * 9, item, e -> new ItemsInventory(0, page, ymlFile).open((Player) e.getWhoClicked()));
+            ItemStack itemStack = null;
+            String material = settings.getString(String.format("gui_inventory.%s", FilenameUtils.removeExtension(ymlFile.getName())), "PAPER");
+            if (material == null) material = "PAPER";
+
+            try {
+                ItemStack isOraxenItems = OraxenItems.getItemById(material).build();
+                if (isOraxenItems != null && isOraxenItems.getType() != null) {
+                    itemStack = new ItemBuilder(isOraxenItems).setDisplayName(ChatColor.GREEN + ymlFile.getName()).build();
+                }
+            } catch (Exception e) {
+                try {
+                    itemStack = new ItemBuilder(Material.getMaterial(material.toUpperCase())).setDisplayName(ChatColor.GREEN + ymlFile.getName()).build();
+                } catch (Exception ignored) {
+                    itemStack = new ItemBuilder(Material.PAPER).setDisplayName(ChatColor.GREEN + ymlFile.getName()).build();
+                }
+            }
+
+            if (itemStack == null) itemStack = new ItemBuilder(Material.PAPER).setDisplayName(ChatColor.GREEN + ymlFile.getName()).build(); //  Bug possible sir isOraxenItems est dispo mais ne peut être un itemstack. Pourquoi ? on ne sait jamais
+            setItem(i - page * 2 * 9, itemStack, e -> new ItemsInventory(0, page, ymlFile).open((Player) e.getWhoClicked()));
         }
 
         setItem(getInventory().getSize() - 5,
