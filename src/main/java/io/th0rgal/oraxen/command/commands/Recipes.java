aa0b10fa7318cea10fbf9885b486486446482390
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2020-08-10 16:56:51 +0200

    Improves commands/Recipes indentation

--- src/main/java/io/th0rgal/oraxen/command/commands/Recipes.java
+++ src/main/java/io/th0rgal/oraxen/command/commands/Recipes.java
@@ -21,39 +21,41 @@ import io.th0rgal.oraxen.language.Message;
 import io.th0rgal.oraxen.utils.general.Placeholder;
 
 public class Recipes {
-
     public static CommandInfo build() {
         return new CommandInfo("recipe", info -> {
             Builder<CommandSender> builder = Literal.of(info.getName()).alias(info.getAliases());
-            builder
-                .requires(Conditions
-                    .mixed(Conditions.reqPerm(OraxenPermission.COMMAND_RECIPE), Conditions.player(Message.NOT_PLAYER)))
-                .then(Literal
-                    .of("builder")
-                    .then(Argument
-                        .of("type", RecipeTypeType.TYPE)
-                        .optionally(Argument.of("furnace", SpecificWordType.of("cookingtime", "experience")))
-                        .executes((sender, context) -> {
+            builder.requires(
+                Conditions.mixed(
+                    Conditions.reqPerm(OraxenPermission.COMMAND_RECIPE), 
+                    Conditions.player(Message.NOT_PLAYER))
+                ).then(
+                    Literal.of("builder").then(
+                        Argument.of("type", RecipeTypeType.TYPE).optionally(
+                            Argument.of("furnace", SpecificWordType.of("cookingtime", "experience"))
+                        ).executes((sender, context) -> {
                             // Builder
-                        })))
-                .then(Literal.of("save").executes((sender, context) -> {
-                    // Save
-                }))
-                .then(Literal
-                    .of("show")
-                    .then(Argument
-                        .of("location", SpecificWordType.of("hand", "all"))
-                        .optionally(Argument.of("type", RecipeTypeType.TYPE))
-                        .executes((sender, context) -> {
-                            // Show
-                        })))
-                .executes((sender, context) -> {
+                        })
+                    )
+                ).then(
+                    Literal.of("save").executes((sender, context) -> {
+                        // Save
+                    })
+                ).then(
+                    Literal.of("show").then(
+                        Argument.of("location", SpecificWordType.of("hand", "all")
+                    ).optionally(
+                        Argument.of("type", RecipeTypeType.TYPE)
+                    ).executes((sender, context) -> {
+                        // Show
+                    }))
+                ).executes((sender, context) -> {
                     Language language = LanguageProvider.getLanguageOf(sender);
-                    Message.COMMAND_HELP_INFO_DETAILED.send(sender, language, new Placeholder("name", info.getName()),
-                        new Placeholder("decription", translate(language, info, DescriptionType.DETAILED)));
+                    Message.COMMAND_HELP_INFO_DETAILED.send(sender, language, 
+                        new Placeholder("name", info.getName()), 
+                        new Placeholder("decription", translate(language, info, DescriptionType.DETAILED))
+                    );
                 });
             return builder;
         });
     }
-
 }
