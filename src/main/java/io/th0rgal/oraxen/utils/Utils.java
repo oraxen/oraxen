9598b362a21c7014cbf2a5ed0409dbd10620ab1d
Th0rgal <thomas.marchand@tuta.io>
2020-08-31 09:31:05 +0200

    refactor

--- src/main/java/io/th0rgal/oraxen/utils/Utils.java
+++ src/main/java/io/th0rgal/oraxen/utils/Utils.java
@@ -16,8 +16,7 @@ public class Utils {
 
     public static List<String> toLowercaseList(String... values) {
         ArrayList<String> list = new ArrayList<>();
-        for (int index = 0; index < values.length; index++)
-            list.add(values[index].toLowerCase());
+        for (String value : values) list.add(value.toLowerCase());
         return list;
     }
 
@@ -29,7 +28,7 @@ public class Utils {
 
     public static int getCode(MultipleFacing blockData) {
         final List<BlockFace> properties = Arrays
-            .asList(BlockFace.EAST, BlockFace.WEST, BlockFace.SOUTH, BlockFace.NORTH, BlockFace.DOWN, BlockFace.UP);
+                .asList(BlockFace.EAST, BlockFace.WEST, BlockFace.SOUTH, BlockFace.NORTH, BlockFace.DOWN, BlockFace.UP);
         int sum = 0;
         for (BlockFace blockFace : blockData.getFaces())
             sum += (int) Math.pow(2, properties.indexOf(blockFace));
@@ -38,15 +37,15 @@ public class Utils {
 
     public static JsonObject getBlockstateWhenFields(int code) {
         JsonObject whenJson = new JsonObject();
-        final String[] properties = new String[] { "up", "down", "north", "south", "west", "east" };
+        final String[] properties = new String[]{"up", "down", "north", "south", "west", "east"};
         for (int i = 0; i < properties.length; i++)
             whenJson.addProperty(properties[properties.length - 1 - i], (code & 0x1 << i) != 0);
         return whenJson;
     }
 
     public static void setBlockFacing(MultipleFacing blockData, int code) {
-        final BlockFace[] properties = new BlockFace[] { BlockFace.EAST, BlockFace.WEST, BlockFace.SOUTH,
-                BlockFace.NORTH, BlockFace.DOWN, BlockFace.UP };
+        final BlockFace[] properties = new BlockFace[]{BlockFace.EAST, BlockFace.WEST, BlockFace.SOUTH,
+                BlockFace.NORTH, BlockFace.DOWN, BlockFace.UP};
         for (int i = 0; i < properties.length; i++) {
             blockData.setFace(properties[i], (code & 0x1 << i) != 0);
         }
