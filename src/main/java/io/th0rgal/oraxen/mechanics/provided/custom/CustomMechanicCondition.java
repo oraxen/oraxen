de6553e07f87637fadf9f97e555080ca09dabbfc
Th0rgal <thomas.marchand@tuta.io>
2021-01-13 12:03:18 +0100

    add HasPermission condition

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicCondition.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicCondition.java
@@ -1,14 +1,34 @@
 package io.th0rgal.oraxen.mechanics.provided.custom;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomMechanicCondition {
 
+    private final List<SingleCondition> conditions;
+
     public CustomMechanicCondition(List<String> conditionNames) {
 
+        conditions = new ArrayList<>();
+        for (String conditionString : conditionNames) {
+            String[] conditionFields = conditionString.split(":");
+
+            switch (conditionFields[0]) {
+
+                case "has_permission":
+                    conditions.add(new HasPermission(conditionFields));
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
 
     public boolean passes(CustomMechanicWrapper wrapper) {
+        for (SingleCondition condition : conditions)
+            if (!condition.passes(wrapper))
+                return false;
         return true;
     }
 
@@ -16,10 +36,22 @@ public class CustomMechanicCondition {
 
 abstract class SingleCondition {
 
-    public SingleCondition(String conditionName) {
-
-    }
-
     abstract boolean passes(CustomMechanicWrapper wrapper);
 
+}
+
+class HasPermission extends SingleCondition {
+
+    private final CustomMechanicWrapper.Field target;
+    private final String permission;
+
+    public HasPermission(String[] fields) {
+        this.target = CustomMechanicWrapper.Field.get(fields[1]);
+        this.permission = fields[2];
+    }
+
+    @Override
+    boolean passes(CustomMechanicWrapper wrapper) {
+        return wrapper.getPlayer(target).hasPermission(permission);
+    }
 }
--- src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicWrapper.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicWrapper.java
@@ -1,11 +1,70 @@
 package io.th0rgal.oraxen.mechanics.provided.custom;
 
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CustomMechanicWrapper {
 
-    public CustomMechanicWrapper(ConfigurationSection section) {
 
+    public enum Field {
+        PLAYER("player"),
+        TARGET("target"),
+        SERVER("server"),
+        ALL("all"),
+        CONSOLE("console");
+
+        private final String name;
+        private static final Map<String, Field> ENUM_MAP;
+
+        Field(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        static {
+            Map<String, Field> map = new ConcurrentHashMap<>();
+            for (Field instance : Field.values())
+                map.put(instance.getName().toLowerCase(), instance);
+            ENUM_MAP = Collections.unmodifiableMap(map);
+        }
+
+        public static Field get(String name) {
+            return ENUM_MAP.get(name.toLowerCase());
+        }
+    }
+
+
+    private Player player;
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    private Player target;
+
+    public void setTarget(Player target) {
+        this.target = target;
+    }
+
+    public Player getPlayer(Field field) {
+        switch (field) {
+
+            case PLAYER:
+                return this.player;
+
+            case TARGET:
+                return this.target;
+
+            default:
+                return null;
+        }
     }
 
 }
