ffd7b12bee83b6a7b2b8c43c07713bff0cf159c1
Th0rgal <thomas.marchand@tuta.io>
2021-01-15 10:54:45 +0100

    run listeners

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicAction.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicAction.java
@@ -8,4 +8,8 @@ public class CustomMechanicAction {
 
     }
 
+    public void run(CustomMechanicWrapper wrapper) {
+
+    }
+
 }
--- src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicListeners.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/custom/CustomMechanicListeners.java
@@ -2,7 +2,13 @@ package io.th0rgal.oraxen.mechanics.provided.custom;
 
 import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.mechanics.MechanicsManager;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomMechanicListeners {
 
@@ -38,4 +44,55 @@ abstract class AbstractListener implements Listener {
         this.condition = condition;
     }
 
+}
+
+class ClickListener extends AbstractListener {
+
+    List<Action> actions = new ArrayList<>();
+
+    ClickListener(String[] fields, CustomMechanicAction action, CustomMechanicCondition condition) {
+        super(action, condition);
+        if (fields[1].equals("right"))
+            if (fields[2].equals("air"))
+                actions.add(Action.RIGHT_CLICK_AIR);
+            else if (fields[2].equals("block"))
+                actions.add(Action.RIGHT_CLICK_BLOCK);
+            else {
+                actions.add(Action.RIGHT_CLICK_AIR);
+                actions.add(Action.RIGHT_CLICK_BLOCK);
+            }
+        else if (fields[1].equals("left"))
+            if (fields[2].equals("air"))
+                actions.add(Action.LEFT_CLICK_AIR);
+            else if (fields[2].equals("block"))
+                actions.add(Action.LEFT_CLICK_BLOCK);
+            else {
+                actions.add(Action.LEFT_CLICK_AIR);
+                actions.add(Action.LEFT_CLICK_BLOCK);
+            }
+        else {
+            if (fields[2].equals("air")) {
+                actions.add(Action.RIGHT_CLICK_AIR);
+                actions.add(Action.LEFT_CLICK_AIR);
+            } else if (fields[2].equals("block")) {
+                actions.add(Action.RIGHT_CLICK_BLOCK);
+                actions.add(Action.LEFT_CLICK_BLOCK);
+            } else {
+                actions.add(Action.RIGHT_CLICK_AIR);
+                actions.add(Action.RIGHT_CLICK_BLOCK);
+                actions.add(Action.LEFT_CLICK_AIR);
+                actions.add(Action.LEFT_CLICK_BLOCK);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event) {
+        if (actions.contains(event.getAction())) {
+            CustomMechanicWrapper wrapper = new CustomMechanicWrapper();
+            wrapper.setPlayer(event.getPlayer());
+            condition.passes(wrapper);
+            action.run(wrapper);
+        }
+    }
 }
