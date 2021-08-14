d753e01213a4809e7e4c768c820e60ce705f51cf
Thomas Marchand <me@thomas-marchand.com>
2021-08-14 23:10:01 +0200

    initial hud commit

---
+++ src/main/java/io/th0rgal/oraxen/hud/HudManager.java
@@ -0,0 +1,17 @@
+package io.th0rgal.oraxen.hud;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class HudManager {
+
+
+    public HudManager(final YamlConfiguration fontConfiguration) {
+
+    }
+
+    private void loadGlyphs(final ConfigurationSection section) {
+
+    }
+
+}
