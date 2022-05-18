bcaeb84def2a7e767f8ca114c8a3d4e29ac79a36
ArubikU <102335860+ArubikU@users.noreply.github.com>
2022-05-18 17:19:41 -0500

    Update MythicMobsCompatibility.java

--- src/main/java/io/th0rgal/oraxen/compatibilities/provided/mythicmobs/MythicMobsCompatibility.java
+++ src/main/java/io/th0rgal/oraxen/compatibilities/provided/mythicmobs/MythicMobsCompatibility.java
@@ -9,21 +9,35 @@ import io.th0rgal.oraxen.items.OraxenItems;
 import org.bukkit.event.EventHandler;
 import org.bukkit.inventory.ItemStack;
 
+import io.lumine.mythic.api.adapters.AbstractItemStack;
+import io.lumine.mythic.api.config.MythicLineConfig;
+import io.lumine.mythic.api.drops.DropMetadata;
+import io.lumine.mythic.api.drops.IDrop;
+import io.lumine.mythic.bukkit.adapters.BukkitItemStack;
+
 public class MythicMobsCompatibility extends CompatibilityProvider<MythicBukkit> {
 
     @EventHandler
     public void onMythicDropLoadEvent(MythicDropLoadEvent event) {
         if (event.getDropName().equalsIgnoreCase("oraxen")) {
-            String line = event.getContainer().getLine();
-            if (line.split(" ").length == 4 && OraxenItems.exists(line.split(" ")[1])) {
-                ItemStack od = OraxenItems.getItemById(line.split(" ")[1]).build();
-                ItemDrop itemDrop = new ItemDrop(line, event.getConfig(), new BukkitItemStack(od));
-                event.register(itemDrop);
-            } else if (line.split(" ").length == 3 && OraxenItems.exists(line.split(" ")[2])) {
-                ItemStack od = OraxenItems.getItemById(line.split(" ")[2]).build();
-                ItemDrop itemDrop = new ItemDrop(line, event.getConfig(), new BukkitItemStack(od));
-                event.register(itemDrop);
-            }
+			event.register(new OraxenDropMythic(event.getConfig(), event.getArgument()));
         }
     }
+    
+    public class OraxenDropMythic implements IDrop {
+        private String material;
+        public ItemsAdder(MythicLineConfig config, String argument) {
+            material = config.getString(new String[] {"item", "key", "id"});
+        }
+        public AbstractItemStack getDrop(DropMetadata arg) {
+            if (OraxenItems.exists(material)) {
+                ItemStack od = OraxenItems.getItemById(OraxenItems.exists(material)).build();
+                return new BukkitItemStack(od);
+            } 
+            
+            ItemStack item = new ItemStack(Material.STONE);
+            return new BukkitItemStack(item);
+        }
+}
+
 }
