4476fbc9f232f2d41eea3bb7a796045204210eaf
ArubikU <102335860+ArubikU@users.noreply.github.com>
2022-06-14 12:44:50 -0500

    Update SwapListener.java

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/general/SwapListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/general/SwapListener.java
@@ -13,6 +13,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 
 import io.th0rgal.oraxen.items.OraxenItems;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
+import io.th0rgal.oraxen.mechanics.provided.gameplay.block.BlockMechanic;
 import io.th0rgal.oraxen.mechanics.provided.gameplay.block.BlockMechanicFactory;
 import io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanic;
 import io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanicFactory;
@@ -21,91 +22,99 @@ import io.th0rgal.oraxen.mechanics.provided.gameplay.stringblock.StringBlockMech
 
 import static io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanicListener.getNoteBlockMechanic;
 import static io.th0rgal.oraxen.mechanics.provided.gameplay.stringblock.StringBlockMechanicListener.getStringMechanic;
-
 import java.util.List;
 import java.util.logging.Level;
 
-	public class SwapListener implements Listener {
-		private final MechanicFactory factory;
-		public SwapListener(MechanicFactory factory) {
-			this.factory = factory;
-		}
-		@EventHandler
-		public void onInteract(PlayerInteractEvent event) {
-			Player player = event.getPlayer();
-			Block block = event.getClickedBlock();
+public class SwapListener implements Listener {
+	private final MechanicFactory factory;
 
-			if (event.getAction() != Action.RIGHT_CLICK_BLOCK)
+	public SwapListener(MechanicFactory factory) {
+		this.factory = factory;
+	}
+
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+		Player player = event.getPlayer();
+		Block block = event.getClickedBlock();
+
+		if (block.getType() == Material.NOTE_BLOCK) {
+			NoteBlockMechanic m = getNoteBlockMechanic(block);
+
+			if (m == null)
+				return;
+			if (m.isDirectional())
+				m = (NoteBlockMechanic) factory.getMechanic(m.getDirectional().getParentBlock());
+
+			if (!m.getSection().contains("swap"))
 				return;
 
-			if (block.getType() == Material.NOTE_BLOCK) {
-				NoteBlockMechanic m = getNoteBlockMechanic(block);
-				
-				if(m==null)return;
-				if (m.isDirectional())
-					m = (NoteBlockMechanic) factory.getMechanic(m.getDirectional().getParentBlock());
-				
-				if(!m.getSection().contains("swap")) return;
-				
-				for (String key : m.getSection().getConfigurationSection("swap").getKeys(false)) {
-					Swap swap = new Swap(m.getSection().getConfigurationSection("swap." + key));
-					Boolean swa = false;
-					if ((swap.items.contains("HAND") && event.getPlayer().getInventory().getItemInMainHand() == null)
-							|| (swap.items.contains("HAND")
-									&& event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.AIR))) {
-						swa = true;
-					}
-
-					if (io.th0rgal.oraxen.items.OraxenItems.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()) != null) {
-						if (swap.items.contains(
-								io.th0rgal.oraxen.items.OraxenItems.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()))) {
-							swa = true;
-						}
-					} else if (swap.items.contains(event.getPlayer().getInventory().getItemInMainHand().getType().toString())) {
-						swa = true;
-					}
-					try {
-						BlockMechanicFactory.setBlockModel(block, swap.toblock);
-					}catch(Exception e) {
-						io.th0rgal.oraxen.OraxenPlugin.get().getLogger().log(Level.WARNING, "Model: "+swap.toblock+" Error in config: "+m.getSection().getCurrentPath());
-					}
-
+			for (String key : m.getSection().getConfigurationSection("swap").getKeys(false)) {
+				Swap swap = new Swap(m.getSection().getConfigurationSection("swap." + key));
+				Boolean swa = false;
+				if ((swap.items.contains("HAND") && event.getPlayer().getInventory().getItemInMainHand() == null)
+						|| (swap.items.contains("HAND") && event.getPlayer().getInventory().getItemInMainHand()
+								.getType().equals(Material.AIR))) {
+					swa = true;
 				}
-			}
-			
 
-			if (block.getType() == Material.TRIPWIRE) {
-				StringBlockMechanic m = getStringMechanic(block);
-				
-				if(m==null)return;
-				
-				if(!m.getSection().contains("swap")) return;
-				
-				for (String key : m.getSection().getConfigurationSection("swap").getKeys(false)) {
-					Swap swap = new Swap(m.getSection().getConfigurationSection("swap." + key));
-					Boolean swa = false;
-					if ((swap.items.contains("HAND") && event.getPlayer().getInventory().getItemInMainHand() == null)
-							|| (swap.items.contains("HAND")
-									&& event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.AIR))) {
+				if (io.th0rgal.oraxen.items.OraxenItems
+						.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()) != null) {
+					if (swap.items.contains(io.th0rgal.oraxen.items.OraxenItems
+							.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()))) {
 						swa = true;
 					}
-					if (io.th0rgal.oraxen.items.OraxenItems.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()) != null) {
-						if (swap.items.contains(
-								io.th0rgal.oraxen.items.OraxenItems.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()))) {
-							swa = true;
-						}
-					} else if (swap.items.contains(event.getPlayer().getInventory().getItemInMainHand().getType().toString())) {
-						swa = true;
-					}
-					try {
-						BlockMechanicFactory.setBlockModel(block, swap.toblock);
-					}catch(Exception e) {
-						io.th0rgal.oraxen.OraxenPlugin.get().getLogger().log(Level.WARNING, "Model: "+swap.toblock+" Error in config: "+m.getSection().getCurrentPath());
-					}
-
+				} else if (swap.items
+						.contains(event.getPlayer().getInventory().getItemInMainHand().getType().toString())) {
+					swa = true;
+				}
+				try {
+					NoteBlockMechanicFactory.setBlockModel(block, swap.toblock);
+				} catch (Exception e) {
+					io.th0rgal.oraxen.OraxenPlugin.get().getLogger().log(Level.WARNING,
+							"Model: " + swap.toblock + " Error in config: " + m.getSection().getCurrentPath());
 				}
 
 			}
 		}
 
+		if (block.getType() == Material.TRIPWIRE) {
+			StringBlockMechanic m = getStringMechanic(block);
+
+			if (m == null)
+				return;
+
+			if (!m.getSection().contains("swap"))
+				return;
+
+			for (String key : m.getSection().getConfigurationSection("swap").getKeys(false)) {
+				Swap swap = new Swap(m.getSection().getConfigurationSection("swap." + key));
+				Boolean swa = false;
+				if ((swap.items.contains("HAND") && event.getPlayer().getInventory().getItemInMainHand() == null)
+						|| (swap.items.contains("HAND") && event.getPlayer().getInventory().getItemInMainHand()
+								.getType().equals(Material.AIR))) {
+					swa = true;
+				}
+				if (io.th0rgal.oraxen.items.OraxenItems
+						.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()) != null) {
+					if (swap.items.contains(io.th0rgal.oraxen.items.OraxenItems
+							.getIdByItem(event.getPlayer().getInventory().getItemInMainHand()))) {
+						swa = true;
+					}
+				} else if (swap.items
+						.contains(event.getPlayer().getInventory().getItemInMainHand().getType().toString())) {
+					swa = true;
+				}
+				try {
+					StringBlockMechanicFactory.setBlockModel(block, swap.toblock);
+				} catch (Exception e) {
+
+					io.th0rgal.oraxen.OraxenPlugin.get().getLogger().log(Level.WARNING,
+							"Model: " + swap.toblock + " Error in config: " + m.getSection().getCurrentPath());
+				}
+
+			}
+
+		}
 	}
+
+}
