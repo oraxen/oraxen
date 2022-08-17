bfe83111584025c79e66ef515f942c5b71ef3fbd
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2022-08-17 07:47:00 +0700

    Implement requested change

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanic.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanic.java
@@ -3,16 +3,57 @@ package io.th0rgal.oraxen.mechanics.provided.misc.food;
 import io.th0rgal.oraxen.mechanics.Mechanic;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class FoodMechanic extends Mechanic {
 
+    private final Set<PotionEffect> effects = new HashSet<>();
     public final int hunger;
     public final int saturation;
+    public final boolean hasReplacement;
+    public boolean isVanillaItem;
+
+    public String item;
+
+    public final boolean hasEffect;
 
     public FoodMechanic(MechanicFactory mechanicFactory, ConfigurationSection section) {
         super(mechanicFactory, section);
         hunger = section.getInt("hunger");
         saturation = section.getInt("saturation");
+        if (section.isConfigurationSection("replacement")) {
+            ConfigurationSection replacementSection = section.getConfigurationSection("replacement");
+            hasReplacement = true;
+            isVanillaItem = replacementSection.getBoolean("isVanillaItem");
+            item = replacementSection.getString("item");
+        }
+        else {
+            hasReplacement = false;
+        }
+        if (section.isConfigurationSection("effects")) {
+            ConfigurationSection effectSection = section.getConfigurationSection("replacement");
+            hasEffect = true;
+            for (String sect : effectSection.getKeys(false)) {
+                registerEffect(effectSection);
+            }
+        } else hasEffect = false;
+    }
+
+    public void registerEffect(ConfigurationSection section) {
+        PotionEffectType effectType = PotionEffectType.getByName(section.getName());
+        int duration = 0;
+        int amplifier = 0;
+        if (section.isInt("duration"))
+            duration = section.getInt("duration");
+        if (section.isInt("amplifier"))
+            amplifier = section.getInt("amplifier");
+        PotionEffect potionEffect = new PotionEffect(effectType, duration, amplifier);
+        effects.add(potionEffect);
     }
 
     public int getHunger() {
@@ -21,5 +62,25 @@ public class FoodMechanic extends Mechanic {
 
     public int getSaturation() {
         return saturation;
+    }
+
+    public boolean hasReplacement() {
+        return hasReplacement;
+    }
+
+    public boolean isVanillaItem() {
+        return isVanillaItem;
+    }
+
+    public String getReplacementItem() {
+        return item;
+    }
+
+    public boolean isHasEffect() {
+        return hasEffect;
+    }
+
+    public Set<PotionEffect> getEffects() {
+        return effects;
     }
 }
--- src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanicListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanicListener.java
@@ -2,10 +2,15 @@ package io.th0rgal.oraxen.mechanics.provided.misc.food;
 
 import io.th0rgal.oraxen.items.OraxenItems;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+
+import java.util.Objects;
 
 public class FoodMechanicListener implements Listener {
     private FoodMechanicFactory factory;
@@ -20,21 +25,26 @@ public class FoodMechanicListener implements Listener {
         String itemID = OraxenItems.getIdByItem(event.getItem());
         if (itemID == null || factory.isNotImplementedIn(itemID))
             return;
+        if (player.getGameMode() == GameMode.CREATIVE)
+            return;
         FoodMechanic mechanic = (FoodMechanic) factory.getMechanic(itemID);
         event.setCancelled(true);
-        if (player.getGameMode() != GameMode.CREATIVE)
+        if (!mechanic.hasReplacement())
             player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+        else if (mechanic.hasReplacement()) {
+            if (mechanic.isVanillaItem() && mechanic.getReplacementItem() != null)
+                player.getInventory().setItemInMainHand(new ItemStack(Objects.requireNonNull(Material.getMaterial(mechanic.getReplacementItem()))));
+            else if (!mechanic.isVanillaItem() && mechanic.getReplacementItem() != null)
+                player.getInventory().setItemInMainHand(OraxenItems.getItemById(mechanic.getReplacementItem()).build());
+        }
+        if (mechanic.isHasEffect() && !mechanic.getEffects().isEmpty()) {
+            for (PotionEffect effect : mechanic.getEffects()) {
+                player.addPotionEffect(effect);
+            }
+        }
         int foodLevel = mechanic.getHunger();
         int saturation = mechanic.getSaturation();
-        if (foodLevel > 20)
-            foodLevel = 20;
-        else if (foodLevel < 0)
-            foodLevel = 0;
-        if (saturation > 20)
-            saturation = 20;
-        else if (saturation < 0)
-            saturation = 0;
-        player.setFoodLevel(player.getFoodLevel() + foodLevel);
-        player.setSaturation(player.getSaturation() + saturation);
+        player.setFoodLevel(player.getFoodLevel() + Math.min(foodLevel, 20));
+        player.setSaturation(player.getSaturation() + Math.min(saturation, 20));
     }
 }
