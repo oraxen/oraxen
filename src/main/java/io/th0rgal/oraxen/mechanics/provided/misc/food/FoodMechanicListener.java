cfa4c6aca409d57530f318cb37318fb8cddaea1f
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2022-08-31 20:07:47 +0700

    Various Quality of Life Improvements (#436)
    * Push
    
    * Revert oraxen.json
    
    * Add food mechanic
    
    Allow custom consume-able items to have custom food level and saturation
    
    * Added random place properties to string block, other impr
    
    - instead of multiply with light level, people can now define how many tick it will boost (delay + boost tick), same applies to rain boost
    - random place for string block, define a list of oraxen string block and when placing the block it will pick a random block in that list and place it instead
    - renamed damage variable to make it easier to understand
    
    * Quick change
    
    * some cleanup
    
    * Implement requested change
    
    * cleanup
    
    * fix food mechanic
    
    * changes to directional blocks
    
    * improvements and cleanup for directional blocks
    
    * tweak directional
    
    * fix directional block resourcepack generation
    
    * final directional block tweaks
    
    * make food be able to take multiple effects
    
    * Restore Mushroom Stem based blocks (part 1)
    
    Give it also new features such as directional, farm block and log strip
    
    * Revert "Restore Mushroom Stem based blocks (part 1)"
    
    This reverts commit 3186cf4f6cf03045327496c18d8997f73145baf6.
    
    Co-authored-by: Boy <sivertpaulsen2@gmail.com>

---
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanicListener.java
@@ -0,0 +1,39 @@
+package io.th0rgal.oraxen.mechanics.provided.misc.food;
+
+import io.th0rgal.oraxen.items.OraxenItems;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.PlayerInventory;
+
+public class FoodMechanicListener implements Listener {
+    private final FoodMechanicFactory factory;
+
+    public FoodMechanicListener(FoodMechanicFactory factory) {
+        this.factory = factory;
+    }
+
+    @EventHandler
+    public void onEatingFood(PlayerItemConsumeEvent event) {
+        Player player = event.getPlayer();
+        PlayerInventory inventory = player.getInventory();
+        String itemID = OraxenItems.getIdByItem(event.getItem());
+        if (itemID == null || factory.isNotImplementedIn(itemID)) return;
+        FoodMechanic mechanic = (FoodMechanic) factory.getMechanic(itemID);
+        event.setCancelled(true);
+
+        if (player.getGameMode() != GameMode.CREATIVE) {
+            if (mechanic.hasReplacement())
+                inventory.setItemInMainHand(mechanic.getReplacement());
+            else inventory.getItemInMainHand().setAmount(inventory.getItemInMainHand().getAmount() - 1);
+
+            if (mechanic.hasEffects() && Math.random() <= mechanic.getEffectProbability())
+                player.addPotionEffects(mechanic.getEffects());
+        }
+
+        player.setFoodLevel(player.getFoodLevel() + Math.min(mechanic.getHunger(), 20));
+        player.setSaturation(player.getSaturation() + Math.min(mechanic.getSaturation(), 20));
+    }
+}
