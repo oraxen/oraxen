080eabe19e635c0d388ce93978c820e2fd6bb071
Boy <sivertpaulsen2@gmail.com>
2022-09-01 23:29:57 +0200

    backpack mechanic

---
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/backpack/BackpackMechanicFactory.java
@@ -0,0 +1,23 @@
+package io.th0rgal.oraxen.mechanics.provided.misc.backpack;
+
+import io.th0rgal.oraxen.OraxenPlugin;
+import io.th0rgal.oraxen.mechanics.Mechanic;
+import io.th0rgal.oraxen.mechanics.MechanicFactory;
+import io.th0rgal.oraxen.mechanics.MechanicsManager;
+import org.bukkit.configuration.ConfigurationSection;
+
+public class BackpackMechanicFactory extends MechanicFactory {
+
+        public BackpackMechanicFactory(ConfigurationSection section) {
+            super(section);
+            MechanicsManager.registerListeners(OraxenPlugin.get(), new BackpackListener(this));
+        }
+
+        @Override
+        public Mechanic parse(ConfigurationSection itemMechanicConfiguration) {
+            Mechanic mechanic = new BackpackMechanic(this, itemMechanicConfiguration);
+            addToImplemented(mechanic);
+            return mechanic;
+        }
+
+}
