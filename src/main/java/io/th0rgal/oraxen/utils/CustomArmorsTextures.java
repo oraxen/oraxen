3907944c69277c67e01c9deb7735336f0a86a108
Boy <sivertpaulsen2@gmail.com>
2022-09-02 16:10:16 +0200

    generate files

--- src/main/java/io/th0rgal/oraxen/utils/CustomArmorsTextures.java
+++ src/main/java/io/th0rgal/oraxen/utils/CustomArmorsTextures.java
@@ -11,10 +11,12 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CustomArmorsTextures {
 
@@ -46,6 +48,9 @@ public class CustomArmorsTextures {
         if (!name.endsWith(".png"))
             return false;
 
+        if (name.startsWith("leather_layer") && !name.contains("overlay"))
+            generateShaderArmor(file, name);
+
         if (name.equals("leather_layer_1.png")) {
             layer1 = initLayer(ImageIO.read(file));
             layer1Width += layer1.getWidth();
@@ -57,6 +62,8 @@ public class CustomArmorsTextures {
             layer2Width += layer2.getWidth();
             return true;
         }
+
+        generateShaderArmor(file, name);
 
         return name.contains("armor_layer_") && handleArmorLayer(name, file);
     }
@@ -103,7 +110,36 @@ public class CustomArmorsTextures {
             setPixel(image.getRaster(), 2, 0, Color.fromRGB(1, 0, 0));
         }
         addPixel(image, builder, name, prefix);
+
         return true;
+    }
+
+    public VirtualFile generateShaderArmor(File file, String name) throws IOException {
+        List<String> splitName = List.of(name.split("_"));
+        Path optifinePath = Path.of("plugins/Oraxen/pack/optifine/cit/armors/", splitName.get(0));
+        File optifineFile = new File(optifinePath + "/" + name);
+        if (!optifinePath.toFile().exists()) optifinePath.toFile().mkdirs();
+        Files.copy(file.toPath(), optifineFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        String cmdProperty = OraxenItems.getEntries().stream().filter(e ->
+                e.getKey().contains(splitName.get(0)) && e.getValue().getOraxenMeta().getLayers().size() == 2
+        ).findFirst().map(entry -> "nbt.CustomModelData=" + entry.getValue().getOraxenMeta().getCustomModelData()).orElse("");
+
+        String armorName = optifineFile.getName().replace("armor_layer", "layer");
+        File optifineProperties = new File(optifinePath + "/" + splitName.get(0) + "_" + splitName.get(1) + ".properties");
+        List<String> propertiesContent = Arrays.asList(
+                "type=armor",
+                "items=minecraft:leather_helmet minecraft:leather_chestplate minecraft:leather_leggings minecraft:leather_boots",
+                "texture.leather_layer_1=" + armorName.replace("_2.png", "_1.png"),
+                "texture.leather_layer_1_overlay=" + armorName.replace("_2.png", "_1.png"),
+                "texture.leather_layer_2=" + armorName.replace("_1.png", "_2.png"),
+                "texture.leather_layer_2_overlay=" + armorName.replace("_1.png", "_2.png"),
+                cmdProperty
+        );
+        if (optifineProperties.exists()) optifineProperties.delete();
+        Files.write(optifineProperties.toPath(), propertiesContent, StandardCharsets.UTF_8);
+
+        return new VirtualFile(optifinePath + "/", splitName.get(0), new FileInputStream(optifineProperties));
     }
 
     private void addPixel(BufferedImage image, ItemBuilder builder, String name, String prefix) {
@@ -136,6 +172,14 @@ public class CustomArmorsTextures {
 
     public InputStream getLayerTwo() throws IOException {
         return getInputStream(layer2Width, getLayerHeight(), layer2, layers2);
+    }
+
+    public List<BufferedImage> getAllLayerOnes() {
+        return layers1;
+    }
+
+    public List<BufferedImage> getAllLayerTwos() {
+        return layers2;
     }
 
     private InputStream getInputStream(int layerWidth, int layerHeight,
