79a814bc0786b661120d8723db72baf8a861b7ce
Boy <sivertpaulsen2@gmail.com>
2022-09-02 18:05:05 +0200

    fix storing data

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/backpack/BackpackListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/backpack/BackpackListener.java
@@ -9,14 +9,19 @@ import io.th0rgal.oraxen.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import static io.th0rgal.oraxen.mechanics.provided.misc.backpack.BackpackMechanic.BACKPACK_KEY;
@@ -24,9 +29,18 @@ import static io.th0rgal.oraxen.mechanics.provided.misc.backpack.BackpackMechani
 public class BackpackListener implements Listener {
 
     private final BackpackMechanicFactory factory;
+    private final Map<Player, Gui> openBackpacks = new HashMap<>();
 
     public BackpackListener(BackpackMechanicFactory factory) {
         this.factory = factory;
+    }
+
+
+    // Cancels placing backpacks if the base material is a block
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onBlockPlace(final BlockPlaceEvent event) {
+        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+        if (isBackpack(item)) event.setCancelled(true);
     }
 
     @EventHandler
@@ -45,13 +59,22 @@ public class BackpackListener implements Listener {
         }
         event.setCancelled(true);
         BackpackMechanic mechanic = (BackpackMechanic) factory.getMechanic(id);
-        createGUI(mechanic, item).open(player);
-        if (mechanic.hasOpenSound())
-            player.playSound(player.getLocation(), mechanic.getOpenSound(), mechanic.getVolume(), mechanic.getPitch());
+        Gui gui = createGUI(mechanic, item);
+        if (gui == null) return;
+        gui.open(player);
+    }
+
+    @EventHandler
+    public void onPickupItem(final PlayerAttemptPickupItemEvent event) {
+        Bukkit.broadcastMessage("test");
+        //openBackpacks.get(event.getPlayer()).update();
     }
 
     private Gui createGUI(BackpackMechanic mechanic, ItemStack backpack) {
-        PersistentDataContainer pdc = Objects.requireNonNull(backpack.getItemMeta()).getPersistentDataContainer();
+        ItemMeta backpackMeta = backpack.getItemMeta();
+        if (!backpack.hasItemMeta()) return null;
+        assert backpackMeta != null;
+        PersistentDataContainer pdc = backpackMeta.getPersistentDataContainer();
         Gui gui = Gui.gui().type(GuiType.CHEST).title(Utils.MINI_MESSAGE.deserialize(mechanic.getTitle())).rows(mechanic.getRows()).create();
 
         gui.disableItemDrop();
@@ -74,16 +97,19 @@ public class BackpackListener implements Listener {
         });
 
         gui.setOpenGuiAction(event -> {
+            Player player = (Player) event.getPlayer();
+            openBackpacks.put(player, gui);
             ItemStack[] contents = pdc.get(BACKPACK_KEY, DataType.ITEM_STACK_ARRAY);
             if (contents != null) gui.getInventory().setContents(contents);
+            if (mechanic.hasOpenSound())
+                player.playSound(player.getLocation(), mechanic.getOpenSound(), mechanic.getVolume(), mechanic.getPitch());
         });
 
         gui.setCloseGuiAction(event -> {
             Player player = (Player) event.getPlayer();
-            Bukkit.broadcastMessage("Content: " + Arrays.toString(gui.getInventory().getContents()));
-            Bukkit.broadcastMessage("hasBackpackKey: " + pdc.has(BACKPACK_KEY, DataType.ITEM_STACK_ARRAY));
+            openBackpacks.remove(player);
             pdc.set(BACKPACK_KEY, DataType.ITEM_STACK_ARRAY, gui.getInventory().getContents());
-            Bukkit.broadcastMessage("hasBackpackKey2: " + pdc.has(BACKPACK_KEY, DataType.ITEM_STACK_ARRAY));
+            backpack.setItemMeta(backpackMeta);
             if (mechanic.hasCloseSound())
                 player.getWorld().playSound(player.getLocation(), mechanic.getCloseSound(), mechanic.getVolume(), mechanic.getPitch());
         });
