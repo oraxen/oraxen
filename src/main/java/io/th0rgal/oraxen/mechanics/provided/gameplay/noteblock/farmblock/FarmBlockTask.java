16df4ab254ca9a4980f437008d517add6987a6a0
TheDeadSnake <60078424+TheDeadSnake@users.noreply.github.com>
2022-10-14 18:14:18 +0200

    (Mostly) Rewrote the dryout check (#515)

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/noteblock/farmblock/FarmBlockTask.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/noteblock/farmblock/FarmBlockTask.java
@@ -6,10 +6,8 @@ import io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanic
 import io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanicFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -18,57 +16,51 @@ import static io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockM
 import static io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanicListener.getNoteBlockMechanic;
 
 public class FarmBlockTask extends BukkitRunnable {
-
     private final int delay;
 
     public FarmBlockTask(int delay) {
         this.delay = delay;
     }
 
+    private boolean isAreaWet(FarmBlockDryout mechanic, Block block, PersistentDataContainer pdc) {
+        boolean nearWater = mechanic.isConnectedToWaterSource(block, pdc);
+        boolean rainingAtBlock = block.getWorld().hasStorm() && block.getWorld().getHighestBlockAt(block.getLocation()) == block;
+        return nearWater || rainingAtBlock;
+    }
+
+    private void updateBlockModel(Block block, PersistentDataContainer pdc, String model) {
+        pdc.remove(FARMBLOCK_KEY);
+        NoteBlockMechanicFactory.setBlockModel(block, model);
+    }
+
+    private void updateBlock(Block block, PersistentDataContainer pdc) {
+        if (pdc.getKeys().contains(FARMBLOCK_KEY)) {
+            NoteBlockMechanic mechanic = getNoteBlockMechanic(block);
+            if (mechanic != null && mechanic.hasDryout()) {
+                FarmBlockDryout farmMechanic = mechanic.getDryout();
+
+                if (isAreaWet(farmMechanic, block, pdc)) {
+                    if (pdc.has(FARMBLOCK_KEY, PersistentDataType.STRING))
+                        updateBlockModel(block, pdc, farmMechanic.getMoistFarmBlock());
+                    pdc.set(FARMBLOCK_KEY, PersistentDataType.INTEGER, 0);
+                } else if (pdc.has(FARMBLOCK_KEY, PersistentDataType.INTEGER)) {
+                    int moistTimerRemain = pdc.get(FARMBLOCK_KEY, PersistentDataType.INTEGER) + delay;
+                    if (farmMechanic.getDryoutTime() - moistTimerRemain <= 0) {
+                        updateBlockModel(block, pdc, farmMechanic.getFarmBlock());
+                        pdc.set(FARMBLOCK_KEY, PersistentDataType.STRING, farmMechanic.getFarmBlock());
+                    } else
+                        pdc.set(FARMBLOCK_KEY, PersistentDataType.INTEGER, moistTimerRemain);
+                }
+            } else //Remove FARMBLOCK_KEY from pdc if the block has no (longer a) dryout mechanic
+                pdc.remove(FARMBLOCK_KEY);
+        }
+    }
+
     @Override
     public void run() {
-        for (World world : Bukkit.getWorlds()) {
-            for (Chunk chunk : world.getLoadedChunks()) {
-                for (Block block : CustomBlockData.getBlocksWithCustomData(OraxenPlugin.get(), chunk)) {
-                    PersistentDataContainer customBlockData = new CustomBlockData(block, OraxenPlugin.get());
-                    if (customBlockData.has(FARMBLOCK_KEY, PersistentDataType.STRING)) {
-                        NoteBlockMechanic mechanic = getNoteBlockMechanic(block);
-                        if (mechanic != null && mechanic.hasDryout()) {
-                            Block blockBelow = block.getRelative(BlockFace.DOWN);
-                            FarmBlockDryout farmMechanic = mechanic.getDryout();
-
-                            if (customBlockData.has(FARMBLOCK_KEY, PersistentDataType.INTEGER)) {
-                                int moistTimerRemain = customBlockData.get(FARMBLOCK_KEY, PersistentDataType.INTEGER) + delay;
-
-                                if (blockBelow.getType() == Material.WATER) {
-                                    customBlockData.set(FARMBLOCK_KEY, PersistentDataType.INTEGER, 0);
-                                    return;
-                                }
-
-                                if (farmMechanic.getDryoutTime() - moistTimerRemain <= 0) {
-                                    NoteBlockMechanicFactory.setBlockModel(block, farmMechanic.getFarmBlock());
-                                    customBlockData.remove(FARMBLOCK_KEY);
-                                    customBlockData.set(FARMBLOCK_KEY, PersistentDataType.STRING, farmMechanic.getFarmBlock());
-                                } else customBlockData.set(FARMBLOCK_KEY, PersistentDataType.INTEGER, moistTimerRemain);
-                            }
-
-                            else {
-                                boolean isWet = !farmMechanic.isMoistFarmBlock() && farmMechanic.isConnectedToWaterSource(block, customBlockData);
-                                boolean rainingAtBlock = world.hasStorm() && world.getHighestBlockAt(block.getLocation()) == block;
-                                if (isWet || rainingAtBlock) {
-                                    NoteBlockMechanicFactory.setBlockModel(block, farmMechanic.getMoistFarmBlock());
-                                    customBlockData.set(FARMBLOCK_KEY, PersistentDataType.INTEGER, 0);
-                                }
-                            }
-                        }
-                        // Remove leftover farmblock pdcs if theyre not a farmblock anymore
-                        // Also covers blocks that arent even custom blocks
-                        else if (mechanic == null || !mechanic.hasDryout()) {
-                            customBlockData.remove(FARMBLOCK_KEY);
-                        }
-                    }
-                }
-            }
-        }
+        for (World world : Bukkit.getWorlds())
+            for (Chunk chunk : world.getLoadedChunks())
+                CustomBlockData.getBlocksWithCustomData(OraxenPlugin.get(), chunk).forEach(block ->
+                        updateBlock(block, new CustomBlockData(block, OraxenPlugin.get())));
     }
 }
