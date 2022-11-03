95d338ae3edce9f545a568cac9cb8f70ce174af0
Boy <sivertpaulsen2@gmail.com>
2022-11-03 16:35:18 +0100

    fix stringblock dropping in creative

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/stringblock/StringBlockMechanicListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/stringblock/StringBlockMechanicListener.java
@@ -426,7 +426,8 @@ public class StringBlockMechanicListener implements Listener {
 
         if (mechanic.getLight() != -1)
             WrappedLightAPI.removeBlockLight(block.getLocation());
-        mechanic.getDrop().spawns(block.getLocation(), item);
+        if (player != null && player.getGameMode() != GameMode.CREATIVE)
+            mechanic.getDrop().spawns(block.getLocation(), item);
         block.setType(Material.AIR, false);
         final Block blockAbove = block.getRelative(BlockFace.UP);
         Bukkit.getScheduler().runTaskLater(OraxenPlugin.get(), () -> {
