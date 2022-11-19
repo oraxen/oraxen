8053b54140b4f44a9f99805b9fe77a713f4952cc
Boy <sivertpaulsen2@gmail.com>
2022-11-19 12:19:54 +0100

    fix some armorrepair nullpointers

--- src/main/java/io/th0rgal/oraxen/utils/customarmor/CustomArmorListener.java
+++ src/main/java/io/th0rgal/oraxen/utils/customarmor/CustomArmorListener.java
@@ -8,8 +8,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitTask;
@@ -22,21 +22,25 @@ public class CustomArmorListener implements Listener {
     private final Map<Player, BukkitTask> taskMap = new HashMap<>();
     
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onCustomArmorRepair(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
+    public void onCustomArmorRepair(InventoryOpenEvent event) {
+        Player player = (Player) event.getPlayer();
         if (taskMap.containsKey(player)) return;
         BukkitTask task = Bukkit.getScheduler().runTaskTimer(OraxenPlugin.get(), () -> {
             if (!(player.getOpenInventory().getTopInventory() instanceof AnvilInventory anvilInventory)) return;
-            ItemStack first = event.getSlot() == 0 ? event.getCurrentItem() : anvilInventory.getItem(0);
-            ItemStack second = event.getSlot() == 1 ? event.getCurrentItem() : anvilInventory.getItem(1);
+            ItemStack first = anvilInventory.getItem(0);
+            ItemStack second = anvilInventory.getItem(1);
+            String firstID = OraxenItems.getIdByItem(first);
+            String secondID = OraxenItems.getIdByItem(second);
 
-            if (first == null || second == null) return;
+            if (first == null || second == null) return; // Empty slot
+            if (firstID == null) return; // Not a custom item
             Material type = first.getType();
             if (type != Material.LEATHER_HELMET && type != Material.LEATHER_CHESTPLATE && type != Material.LEATHER_LEGGINGS && type != Material.LEATHER_BOOTS)
-                return;
+                return; // Not a custom armor
 
+            // Set result next tick
             Bukkit.getScheduler().runTaskLater(OraxenPlugin.get(), () -> {
-                if (!OraxenItems.getIdByItem(first).equals(OraxenItems.getIdByItem(second)) || !OraxenItems.exists(first) || second.getType() == Material.LEATHER)
+                if (!firstID.equals(secondID) || second.getType() == Material.LEATHER)
                     anvilInventory.setItem(2, new ItemStack(Material.AIR));
             }, 1L);
         }, 2L, 2L);
