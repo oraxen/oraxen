e4281011b7cb180ff75a4a6987bb1e15f3eee90b
Boy <sivertpaulsen2@gmail.com>
2022-12-28 18:37:50 +0100

    fix disable_leather_armor_repair

--- src/main/java/io/th0rgal/oraxen/utils/customarmor/CustomArmorListener.java
+++ src/main/java/io/th0rgal/oraxen/utils/customarmor/CustomArmorListener.java
@@ -1,66 +1,36 @@
 package io.th0rgal.oraxen.utils.customarmor;
 
-import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.api.OraxenItems;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitTask;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class CustomArmorListener implements Listener {
-    
-    private final Map<Player, BukkitTask> taskMap = new HashMap<>();
-    
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onCustomArmorRepair(InventoryOpenEvent event) {
-        Player player = (Player) event.getPlayer();
-        if (taskMap.containsKey(player)) return;
-        BukkitTask task = Bukkit.getScheduler().runTaskTimer(OraxenPlugin.get(), () -> {
-            if (!(player.getOpenInventory().getTopInventory() instanceof AnvilInventory anvilInventory)) return;
-            ItemStack first = anvilInventory.getItem(0);
-            ItemStack second = anvilInventory.getItem(1);
-            String firstID = OraxenItems.getIdByItem(first);
-            String secondID = OraxenItems.getIdByItem(second);
 
-            if (first == null || second == null) return; // Empty slot
-            if (firstID == null) return; // Not a custom item
-            Material type = first.getType();
-            if (type != Material.LEATHER_HELMET && type != Material.LEATHER_CHESTPLATE && type != Material.LEATHER_LEGGINGS && type != Material.LEATHER_BOOTS)
-                return; // Not a custom armor
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onCustomArmorRepair(PrepareAnvilEvent event) {
+        AnvilInventory inventory = event.getInventory();
+        Player player = (Player) inventory.getViewers().stream().filter(p -> p.getOpenInventory().getTopInventory() == inventory).findFirst().get();
 
-            // Set result next tick
-            Bukkit.getScheduler().runTaskLater(OraxenPlugin.get(), () -> {
-                if (!firstID.equals(secondID) || second.getType() == Material.LEATHER)
-                    anvilInventory.setItem(2, new ItemStack(Material.AIR));
-            }, 1L);
-        }, 2L, 2L);
-        taskMap.putIfAbsent(player, task);
+        ItemStack first = inventory.getItem(0);
+        ItemStack second = inventory.getItem(1);
+        String firstID = OraxenItems.getIdByItem(first);
+        String secondID = OraxenItems.getIdByItem(second);
 
-    }
+        if (first == null || second == null) return; // Empty slot
+        if (firstID == null) return; // Not a custom item
+        Material type = first.getType();
+        if (type != Material.LEATHER_HELMET && type != Material.LEATHER_CHESTPLATE && type != Material.LEATHER_LEGGINGS && type != Material.LEATHER_BOOTS)
+            return; // Not a custom armor
 
-    @EventHandler
-    public void onCloseAnvil(InventoryCloseEvent event) {
-        closeTasks((Player) event.getPlayer());
-
-    }
-
-    private void closeTasks(Player player) {
-        if (taskMap.containsKey(player)) {
-            Bukkit.getScheduler().runTaskLater(OraxenPlugin.get(), () -> {
-                BukkitTask task = taskMap.get(player);
-                if (task != null) task.cancel();
-                taskMap.remove(player);
-            }, 1L);
+        if (second.getType() == Material.LEATHER || (!firstID.equals(secondID) && secondID != null)) {
+            event.setResult(null);
+            player.updateInventory();
         }
     }
 }
