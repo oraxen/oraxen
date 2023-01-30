7cf5a971228b9a5961072c953aa26b10fc8b2193
Boy0000 <sivertpaulsen2@gmail.com>
2023-01-30 12:49:51 +0000

    More parent models (#677)

--- src/main/java/io/th0rgal/oraxen/pack/generation/ModelGenerator.java
+++ src/main/java/io/th0rgal/oraxen/pack/generation/ModelGenerator.java
@@ -18,7 +18,15 @@ public class ModelGenerator {
             oraxenMeta.getLayersMap().forEach(textures::addProperty);
         } else if (oraxenMeta.hasLayers()) { //Use old texture system
             List<String> layers = oraxenMeta.getLayers();
-            if (parentModel.equals("block/cube_all")) {
+            if (parentModel.equals("block/cube") || parentModel.equals("block/cube_directional") || parentModel.equals("block/cube_mirrored")) {
+                textures.addProperty("particle", layers.get(2));
+                textures.addProperty("down", layers.get(0));
+                textures.addProperty("up", layers.get(1));
+                textures.addProperty("north", layers.get(2));
+                textures.addProperty("south", layers.get(3));
+                textures.addProperty("west", layers.get(4));
+                textures.addProperty("east", layers.get(5));
+            } else if (parentModel.equals("block/cube_all") || parentModel.equals("block/cube_mirrored_all")) {
                 textures.addProperty("all", layers.get(0));
             } else if (parentModel.equals("block/cross")) {
                 textures.addProperty("cross", layers.get(0));
@@ -27,8 +35,17 @@ public class ModelGenerator {
                 textures.addProperty("side", layers.get(1));
                 if (!parentModel.endsWith("vertical"))
                     textures.addProperty("top", layers.get(2));
-            } else if (parentModel.equals("block/cube_column")) {
+                if (parentModel.endsWith("with_bottom"))
+                    textures.addProperty("bottom", layers.get(3));
+            } else if (parentModel.startsWith("block/cube_column")) {
                 textures.addProperty("end", layers.get(0));
+                textures.addProperty("side", layers.get(1));
+            } else if (parentModel.equals("block/cube_bottom_top")) {
+                textures.addProperty("top", layers.get(0));
+                textures.addProperty("side", layers.get(1));
+                textures.addProperty("bottom", layers.get(2));
+            } else if (parentModel.equals("block/cube_top")) {
+                textures.addProperty("top", layers.get(0));
                 textures.addProperty("side", layers.get(1));
             } else {
                 for (int i = 0; i < layers.size(); i++)
