829c44ebf3e7016993e0ac700b23d1c768e89548
Boy <sivertpaulsen2@gmail.com>
2023-02-09 11:59:42 +0100

    make atlas generation use only sprites, to filter out any non item textures

--- src/main/java/io/th0rgal/oraxen/pack/generation/AtlasGenerator.java
+++ src/main/java/io/th0rgal/oraxen/pack/generation/AtlasGenerator.java
@@ -2,15 +2,15 @@ package io.th0rgal.oraxen.pack.generation;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import io.th0rgal.oraxen.OraxenPlugin;
+import io.th0rgal.oraxen.api.OraxenItems;
 import io.th0rgal.oraxen.utils.Utils;
 import io.th0rgal.oraxen.utils.VirtualFile;
 import io.th0rgal.oraxen.utils.logs.Logs;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AtlasGenerator {
@@ -24,26 +24,42 @@ public class AtlasGenerator {
         JsonObject atlas = new JsonObject();
         JsonArray atlasContent = new JsonArray();
         Map<VirtualFile, String> textureSubFolders = new HashMap<>();
-        for (VirtualFile v : output.stream().filter(v -> v.getPath().split("/").length > 3 && v.getPath().endsWith(".png")).collect(Collectors.toSet()))
-            textureSubFolders.put(v, v.getPath().replaceFirst("assets/.*/textures/", "").split("/")[0]);
+        for (VirtualFile v : output.stream().filter(v ->
+                v.getPath().split("/").length > 3
+                        && v.getPath().split("/")[2].equals("textures")
+                        && v.getPath().endsWith(".png")
+                        && !v.getPath().endsWith("_layer_1.png")
+                        && !v.getPath().endsWith("_layer_2.png")
+        ).collect(Collectors.toSet())) {
+            textureSubFolders.put(v, Utils.removeExtension(v.getPath().replaceFirst("assets/.*/textures/", "")));
+        }
 
-        textureSubFolders.forEach((virtual, path) -> {
+        Set<String> itemTextures = new HashSet<>();
+        OraxenItems.getItems().stream().filter(builder -> builder.hasOraxenMeta() && builder.getOraxenMeta().hasLayers())
+                .forEach(builder -> itemTextures.addAll(builder.getOraxenMeta().getLayers()));
+
+        Set<String> glyphTextures = new HashSet<>();
+        OraxenPlugin.get().getFontManager().getGlyphs()
+                .forEach(glyph -> glyphTextures.add(glyph.getTexture().replace(".png", "")));
+
+        for (Map.Entry<VirtualFile, String> entry : textureSubFolders.entrySet()) {
+            VirtualFile virtual = entry.getKey();
+            String path = entry.getValue();
+
+            // If a texture is for a glyph, do not add it to atlas, unless an item uses it
+            // Default example is required/exit_icon.png
+            if (glyphTextures.contains(path) && !itemTextures.contains(path)) continue;
+
             JsonObject atlasEntry = new JsonObject();
-            if (path.endsWith(".png")) {
-                String namespace = virtual.getPath().replaceFirst("assets/", "").split("/")[0];
-                String sprite = Utils.removeParentDirs(Utils.removeExtension(virtual.getPath()));
-                atlasEntry.addProperty("type", "single");
-                atlasEntry.addProperty("resource", namespace + ":" + Utils.removeExtension(path));
-                atlasEntry.addProperty("sprite", namespace + ":" + sprite);
-            } else {
-                atlasEntry.addProperty("type", "directory");
-                atlasEntry.addProperty("source", path);
-                atlasEntry.addProperty("prefix", path + "/");
-            }
+            String namespace = virtual.getPath().replaceFirst("assets/", "").split("/")[0];
+            String sprite = namespace + ":" + path;
+            atlasEntry.addProperty("type", "single");
+            atlasEntry.addProperty("resource", sprite);
+            atlasEntry.addProperty("sprite", sprite);
 
             if (!atlasContent.contains(atlasEntry))
                 atlasContent.add(atlasEntry);
-        });
+        }
 
         atlas.add("sources", atlasContent);
         VirtualFile atlasFile = new VirtualFile("assets/minecraft/atlases", "blocks.json", new ByteArrayInputStream(atlas.toString().getBytes(StandardCharsets.UTF_8)));
