6cc0dbd564abcb402312a19a4a2e5368d9b679e9
Boy <sivertpaulsen2@gmail.com>
2023-03-26 20:28:00 +0200

    more tweaks to item and font migrators

--- src/main/java/io/th0rgal/oraxen/config/Settings.java
+++ src/main/java/io/th0rgal/oraxen/config/Settings.java
@@ -24,8 +24,6 @@ public enum Settings {
     FORMAT_SIGNS("Plugin.formatting.signs"),
     FORMAT_CHAT("Plugin.formatting.chat"),
     FORMAT_BOOKS("Plugin.formatting.books"),
-    MERGE_ITEM_MODELS("Plugin.experimental.merge_item_base_models"),
-    MERGE_FONTS("Plugin.experimental.merge_font_files"),
 
     CONFIGS_VERSION("configs_version"),
     UPDATE_CONFIGS("ConfigsTools.enable_configs_updater"),
@@ -47,7 +45,6 @@ public enum Settings {
     HIDE_SCOREBOARD_NUMBERS("Misc.hide_scoreboard_numbers"),
 
     GENERATE("Pack.generation.generate"),
-    ATTEMPT_TO_MIGRATE_DUPLICATES("Pack.generation.attempt_to_migrate_duplicates"),
     EXCLUDED_FILE_EXTENSIONS("Pack.generation.excluded_file_extensions"),
     GENERATE_ATLAS_FILE("Pack.generation.generate_atlas_file"),
     GENERATE_MODEL_BASED_ON_TEXTURE_PATH("Pack.generation.auto_generated_models_follow_texture_path"),
@@ -59,6 +56,10 @@ public enum Settings {
     COMPRESSION("Pack.generation.compression"),
     PROTECTION("Pack.generation.protection"),
     COMMENT("Pack.generation.comment"),
+    MERGE_DUPLICATES("Pack.import.merge_duplicates"),
+    RETAIN_CUSTOM_MODEL_DATA("Pack.import.retain_custom_model_data"),
+    MERGE_ITEM_MODELS("Pack.import.merge_item_base_models"),
+    MERGE_FONTS("Pack.import.merge_font_files"),
 
     UPLOAD_TYPE("Pack.upload.type"),
     UPLOAD("Pack.upload.enabled"),
