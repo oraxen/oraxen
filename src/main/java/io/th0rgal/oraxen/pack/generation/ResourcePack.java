4f618ed458eae779095fc4169ad2ff60ebef4096
Boy <sivertpaulsen2@gmail.com>
2023-03-26 21:10:27 +0200

    fix class error again

--- src/main/java/io/th0rgal/oraxen/pack/generation/ResourcePack.java
+++ src/main/java/io/th0rgal/oraxen/pack/generation/ResourcePack.java
@@ -204,7 +204,7 @@ public class ResourcePack {
                         for (JsonElement element : jsonModel.getAsJsonObject("textures").entrySet().stream().map(Map.Entry::getValue).toList()) {
                             String jsonTexture = element.getAsString();
                             if (!texturePaths.contains(modelPathToPackPath(jsonTexture))) {
-                                if (!jsonTexture.startsWith("item/") && !jsonTexture.startsWith("block/")) {
+                                if (!jsonTexture.startsWith("#") && !jsonTexture.startsWith("item/") && !jsonTexture.startsWith("block/")) {
                                     try {
                                         Material.valueOf(Utils.removeParentDirs(Utils.removeExtension(jsonTexture)).toUpperCase());
                                     } catch (IllegalArgumentException e) {
