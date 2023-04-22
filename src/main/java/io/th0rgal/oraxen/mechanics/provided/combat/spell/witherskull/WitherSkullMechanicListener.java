e30df77f5898bdaac2f113da4a11d275f941f57c
Boy <sivertpaulsen2@gmail.com>
2023-04-22 14:20:43 +0200

    fix some mechanics triggering on interacting with blocks

--- src/main/java/io/th0rgal/oraxen/mechanics/provided/combat/spell/witherskull/WitherSkullMechanicListener.java
+++ src/main/java/io/th0rgal/oraxen/mechanics/provided/combat/spell/witherskull/WitherSkullMechanicListener.java
@@ -3,9 +3,12 @@ package io.th0rgal.oraxen.mechanics.provided.combat.spell.witherskull;
 import io.th0rgal.oraxen.api.OraxenItems;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
 import io.th0rgal.oraxen.utils.timers.Timer;
+import io.th0rgal.protectionlib.ProtectionLib;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.WitherSkull;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -24,19 +27,21 @@ public class WitherSkullMechanicListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onCall(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK)
-            return;
-
+        Player player = event.getPlayer();
         ItemStack item = event.getItem();
         String itemID = OraxenItems.getIdByItem(item);
+        WitherSkullMechanic mechanic = (WitherSkullMechanic) factory.getMechanic(item);
+        Block block = event.getClickedBlock();
+        Location location = block != null ? block.getLocation() : player.getLocation();
 
-        if (factory.isNotImplementedIn(itemID))
-            return;
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.useInteractedBlock() == Event.Result.ALLOW) return;
+        if (event.useItemInHand() == Event.Result.DENY) return;
+        if (!ProtectionLib.canUse(player, location)) return;
+        if (factory.isNotImplementedIn(itemID)) return;
+        if (mechanic == null) return;
 
-        WitherSkullMechanic mechanic = (WitherSkullMechanic) factory.getMechanic(itemID);
-        Player player = event.getPlayer();
         Timer playerTimer = mechanic.getTimer(player);
-
         if (!playerTimer.isFinished()) {
             mechanic.getTimer(player).sendToPlayer(player);
             return;
