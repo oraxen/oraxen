7623e8db493db155a55e6b261e21c76af14a1dc6
CrazyDev22 <juliankrings6@gmail.com>
2023-04-25 21:51:15 +0200

    Removed unused imports

--- src/main/java/io/th0rgal/oraxen/recipes/builders/FurnaceBuilder.java
+++ src/main/java/io/th0rgal/oraxen/recipes/builders/FurnaceBuilder.java
@@ -1,11 +1,7 @@
 package io.th0rgal.oraxen.recipes.builders;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 
 public class FurnaceBuilder extends CookingBuilder {
