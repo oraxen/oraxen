0a068f38394a00008a2f17f036afb77377a61b06
CrazyDev22 <juliankrings6@gmail.com>
2023-04-25 22:02:03 +0200

    Apply removed variable inventory type to subclasses

--- src/main/java/io/th0rgal/oraxen/recipes/builders/FurnaceBuilder.java
+++ src/main/java/io/th0rgal/oraxen/recipes/builders/FurnaceBuilder.java
@@ -1,12 +1,12 @@
 package io.th0rgal.oraxen.recipes.builders;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryType;
 
 
 public class FurnaceBuilder extends CookingBuilder {
 
     public FurnaceBuilder(Player player) {
-        super(player, "furnace", InventoryType.FURNACE);
+        super(player, "furnace");
     }
+
 }
