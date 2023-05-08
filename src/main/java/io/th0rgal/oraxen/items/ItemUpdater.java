8fdd64646c3f694018ec96f8a7877ca691548f05
Boy <sivertpaulsen2@gmail.com>
2023-05-08 13:11:27 +0200

    make furniture run in an async task

--- src/main/java/io/th0rgal/oraxen/items/ItemUpdater.java
+++ src/main/java/io/th0rgal/oraxen/items/ItemUpdater.java
@@ -1,11 +1,11 @@
 package io.th0rgal.oraxen.items;
 
 import com.jeff_media.morepersistentdatatypes.DataType;
+import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.api.OraxenFurniture;
 import io.th0rgal.oraxen.api.OraxenItems;
 import io.th0rgal.oraxen.config.Settings;
 import io.th0rgal.oraxen.mechanics.provided.gameplay.durability.DurabilityMechanic;
-import io.th0rgal.oraxen.mechanics.provided.gameplay.furniture.FurnitureMechanic;
 import io.th0rgal.oraxen.mechanics.provided.misc.backpack.BackpackMechanic;
 import io.th0rgal.oraxen.utils.AdventureUtils;
 import io.th0rgal.oraxen.utils.Utils;
@@ -25,13 +25,34 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 
 import static io.th0rgal.oraxen.items.ItemBuilder.ORIGINAL_NAME_KEY;
 
 public class ItemUpdater implements Listener {
+
+    public ItemUpdater() {
+        if (furnitureUpdateTask != null) furnitureUpdateTask.cancel();
+        furnitureUpdateTask = new FurnitureUpdateTask();
+        int delay = (Settings.FURNITURE_UPDATE_DELAY.getValue() instanceof Integer integer) ? integer : 5;
+        furnitureUpdateTask.runTaskTimerAsynchronously(OraxenPlugin.get(), 0, delay * 20L);
+    }
+
+    public static HashSet<Entity> furnitureToUpdate = new HashSet<>();
+    public static FurnitureUpdateTask furnitureUpdateTask;
+    public static class FurnitureUpdateTask extends BukkitRunnable {
+
+        @Override
+        public void run() {
+            for (Entity entity : furnitureToUpdate)
+                OraxenFurniture.updateFurniture(entity);
+            furnitureToUpdate.clear();
+        }
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -60,14 +81,11 @@ public class ItemUpdater implements Listener {
     @EventHandler
     public void onEntityLoad(EntitiesLoadEvent event) {
         if (!Settings.AUTO_UPDATE_ITEMS.toBool()) return;
+        if (!Settings.UPDATE_FURNITURE_ON_LOAD.toBool()) return;
 
-        for (Entity entity : event.getEntities()) {
-            if (!OraxenFurniture.isFurniture(entity)) return;
-            ItemStack oldItem = FurnitureMechanic.getFurnitureItem(entity);
-            ItemStack newItem = ItemUpdater.updateItem(oldItem);
-            if (oldItem == null || oldItem.equals(newItem)) continue;
-            FurnitureMechanic.setFurnitureItem(entity, newItem);
-        }
+        for (Entity entity : event.getEntities())
+            if (OraxenFurniture.isFurniture(entity))
+                ItemUpdater.furnitureToUpdate.add(entity);
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
