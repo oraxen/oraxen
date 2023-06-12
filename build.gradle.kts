9b747bff9f74cac1dce1737a96794ee94babca23
Boy <sivertpaulsen2@gmail.com>
2023-06-12 13:22:33 +0200

    bump protectionlib

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,9 +65,9 @@ dependencies {
 
     implementation("dev.triumphteam:triumph-gui:3.1.5")
     implementation("org.bstats:bstats-bukkit:3.0.0")
-    implementation("com.github.oraxen:protectionlib:1.2.7")
-    implementation("net.kyori:adventure-text-minimessage:4.13.1")
-    implementation("net.kyori:adventure-text-serializer-plain:4.13.1")
+    implementation("com.github.oraxen:protectionlib:1.3.0")
+    implementation("net.kyori:adventure-text-minimessage:4.14.0")
+    implementation("net.kyori:adventure-text-serializer-plain:4.14.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.0")
     implementation("com.github.stefvanschie.inventoryframework:IF:0.10.9")
     implementation("com.jeff_media:CustomBlockData:2.2.0")
