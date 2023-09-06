d7e8be05f6e18d6c5c0efa678eff8c52a9d37285
Boy <sivertpaulsen2@gmail.com>
2023-09-06 14:16:51 +0200

    fix block-place admin command

--- core/src/main/java/io.th0rgal.oraxen/commands/AdminCommands.java
+++ core/src/main/java/io.th0rgal.oraxen/commands/AdminCommands.java
@@ -1,29 +1,16 @@
 package io.th0rgal.oraxen.commands;
 
-import com.sk89q.worldedit.blocks.Blocks;
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.ArgumentSuggestions;
-import dev.jorel.commandapi.arguments.BooleanArgument;
-import dev.jorel.commandapi.arguments.IntegerArgument;
-import dev.jorel.commandapi.arguments.LocationArgument;
-import dev.jorel.commandapi.arguments.TextArgument;
+import dev.jorel.commandapi.arguments.*;
 import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.api.OraxenBlocks;
 import io.th0rgal.oraxen.api.OraxenFurniture;
 import io.th0rgal.oraxen.utils.AdventureUtils;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Rotation;
 import org.bukkit.block.Block;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class AdminCommands {
 
@@ -50,8 +37,9 @@ public class AdminCommands {
                         int radius = (int) args.getOptional("radius").orElse(10);
                         boolean isRandom = (boolean) args.getOptional("random").orElse(false);
                         for (Block block : getBlocks(loc, radius, isRandom)) {
+                            if (type == null) continue;
                             if (type.equals("remove")) OraxenBlocks.remove(block.getLocation(), null);
-                            if (type.equals("place")) OraxenFurniture.place(block.getLocation(), id, Rotation.CLOCKWISE, null);
+                            if (type.equals("place")) OraxenBlocks.place(id, block.getLocation());
                         }
                     }
                 });
