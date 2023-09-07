42848741d39fb9fa41faa5066ba63d02d2caf2f2
Boy <sivertpaulsen2@gmail.com>
2023-09-07 10:49:46 +0200

    remove debug prints

--- core/src/main/java/io.th0rgal.oraxen/font/GlyphTag.java
+++ core/src/main/java/io.th0rgal.oraxen/font/GlyphTag.java
@@ -1,7 +1,6 @@
 package io.th0rgal.oraxen.font;
 
 import io.th0rgal.oraxen.OraxenPlugin;
-import io.th0rgal.oraxen.utils.logs.Logs;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -31,7 +30,6 @@ public class GlyphTag {
     static Tag create(final ArgumentQueue args, final Context ctx, Player player) throws ParsingException {
         String arg = args.popOr("A glyph value is required").value();
         Glyph glyph = OraxenPlugin.get().getFontManager().getGlyphFromName(arg);
-        Logs.debug((player == null || glyph.hasPermission(player)) + " : " + glyph.getName() + " : " + args);
         Component glyphComponent = player == null || glyph.hasPermission(player) ? Component.text(glyph.getCharacter()).font(Key.key("default")).style(Style.empty()) : Component.text(arg);
         if (!args.hasNext() || !args.peek().value().equals("colorable"))
             glyphComponent = glyphComponent.color(NamedTextColor.WHITE);
