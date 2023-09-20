4c10f83e89bf35bc91f99f583f6ff9999e097de6
Boy <sivertpaulsen2@gmail.com>
2023-09-20 17:03:23 +0200

    bump customblockdata

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("dev.triumphteam:triumph-gui:3.1.5")
     implementation("com.github.oraxen:protectionlib:1.3.5")
     implementation("com.github.stefvanschie.inventoryframework:IF:0.10.9")
-    implementation("com.jeff_media:CustomBlockData:2.2.0")
+    implementation("com.jeff-media:custom-block-data:2.2.2")
     implementation("com.jeff_media:MorePersistentDataTypes:2.4.0")
     implementation("com.jeff-media:persistent-data-serializer:1.0")
     implementation("gs.mclo:java:2.2.1")
