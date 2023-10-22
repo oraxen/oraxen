1d260597e7617508d3f2bc94f964d6ab8acc67eb
Boy <sivertpaulsen2@gmail.com>
2023-10-22 14:09:28 +0200

    begin work on adding item pack-content

--- core/src/main/java/io/th0rgal/oraxen/pack/PackGenerator.java
+++ core/src/main/java/io/th0rgal/oraxen/pack/PackGenerator.java
@@ -1,6 +1,9 @@
 package io.th0rgal.oraxen.pack;
 
 import io.th0rgal.oraxen.OraxenPlugin;
+import io.th0rgal.oraxen.api.OraxenItems;
+import io.th0rgal.oraxen.items.ItemBuilder;
+import io.th0rgal.oraxen.items.OraxenMeta;
 import team.unnamed.creative.BuiltResourcePack;
 import team.unnamed.creative.ResourcePack;
 import team.unnamed.creative.serialize.minecraft.MinecraftResourcePackReader;
@@ -22,10 +25,19 @@ public class PackGenerator {
         resourcePack.unknownFiles().remove("DefaultPack.zip");
         resourcePack.unknownFiles().remove("pack.zip");
         MinecraftResourcePackWriter.minecraft().writeToZipFile(OraxenPlugin.get().packPath().resolve("pack.zip").toFile(), resourcePack);
+
         builtPack = MinecraftResourcePackWriter.minecraft().build(resourcePack);
     }
 
     public BuiltResourcePack getBuiltPack() {
         return builtPack;
+    }
+
+    private void addItemPackFiles() {
+        for (ItemBuilder builder : OraxenItems.getItems()) {
+            if (builder == null || !builder.hasOraxenMeta()) continue;
+            OraxenMeta meta = builder.getOraxenMeta();
+            if (meta.getCustomModelData() == 0) return;
+        }
     }
 }
