f4aed9a088082273adddd432ad0be41463d59db9
Boy <sivertpaulsen2@gmail.com>
2023-10-31 19:12:45 +0100

    fix: food-mechanic not registering replacementItem & effects

--- core/src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanic.java
+++ core/src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanic.java
@@ -6,7 +6,9 @@ import io.th0rgal.oraxen.mechanics.Mechanic;
 import io.th0rgal.oraxen.mechanics.MechanicFactory;
 import io.th0rgal.oraxen.utils.logs.Logs;
 import net.Indyuce.mmoitems.MMOItems;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
@@ -30,30 +32,26 @@ public class FoodMechanic extends Mechanic {
         saturation = section.getInt("saturation");
         effectProbability = Math.min(section.getDouble("effect_probability", 1.0), 1.0);
 
-        if (section.isConfigurationSection("replacement")) {
-            ConfigurationSection replacementSection = section.getConfigurationSection("replacement");
-            assert replacementSection != null;
-            registerReplacement(replacementSection);
-        } else replacementItem = null;
+        // This must be initialized in OraxenItemsLoadedEvent due to OraxenItems not being loaded yet
+        replacementItem = section.isConfigurationSection("replacement") ? new ItemStack(Material.AIR) : null;
 
-        if (section.isConfigurationSection("effects")) {
-            ConfigurationSection effectsSection = section.getConfigurationSection("effects");
-            assert effectsSection != null;
-            for (String effectSection : effectsSection.getKeys(false))
-                if (section.isConfigurationSection(effectSection)) {
-                    ConfigurationSection effect = section.getConfigurationSection("effectSection");
-                    assert effect != null;
-                    registerEffects(effect);
-                }
+        ConfigurationSection effectsSection = section.getConfigurationSection("effects");
+        if (effectsSection != null) for (String effect : effectsSection.getKeys(false)) {
+            ConfigurationSection effectSection = effectsSection.getConfigurationSection(effect);
+            if (effectSection != null) registerEffects(effectSection);
         }
     }
 
     private void registerEffects(ConfigurationSection section) {
-        PotionEffectType effectType = PotionEffectType.getByName(section.getName());
+        String type = section.getName().toLowerCase();
+        PotionEffectType effectType = PotionEffectType.getByName(type);
+        if (effectType == null)
+            effectType = PotionEffectType.getByKey(type.contains(":") ? NamespacedKey.fromString(type) : NamespacedKey.minecraft(type));
         if (effectType == null) {
-            Logs.logError("Invalid effect type: " + section.getName() + " in " + getItemID());
+            Logs.logError("Invalid potion effect: " + section.getName() + ", in " + StringUtils.substringBefore(section.getCurrentPath(), ".") + "!");
             return;
         }
+
         effects.add(new PotionEffect(effectType,
                 section.getInt("duration", 1) * 20,
                 section.getInt("amplifier", 0),
@@ -62,14 +60,13 @@ public class FoodMechanic extends Mechanic {
                 section.getBoolean("has_icon", true)));
     }
 
-    private void registerReplacement(ConfigurationSection section) {
+    public void registerReplacement(ConfigurationSection section) {
         if (section.isString("minecraft_type")) {
             Material material = Material.getMaterial(Objects.requireNonNull(section.getString("minecraft_type")));
             if (material == null) {
                 Logs.logError("Invalid material: " + section.getString("minecraft_type"));
                 replacementItem = null;
-            }
-            else replacementItem = new ItemStack(material);
+            } else replacementItem = new ItemStack(material);
         } else if (section.isString("oraxen_item"))
             replacementItem = OraxenItems.getItemById(section.getString("oraxen_item")).build();
         else if (section.isString("crucible_item"))
@@ -95,8 +92,13 @@ public class FoodMechanic extends Mechanic {
         return replacementItem;
     }
 
-    public boolean hasEffects() { return !effects.isEmpty(); }
-    public Set<PotionEffect> getEffects() { return effects; }
+    public boolean hasEffects() {
+        return !effects.isEmpty();
+    }
+
+    public Set<PotionEffect> getEffects() {
+        return effects;
+    }
 
     public double getEffectProbability() {
         return effectProbability;
--- core/src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanicListener.java
+++ core/src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/food/FoodMechanicListener.java
@@ -1,9 +1,13 @@
 package io.th0rgal.oraxen.mechanics.provided.misc.food;
 
 import io.th0rgal.oraxen.api.OraxenItems;
+import io.th0rgal.oraxen.api.events.OraxenItemsLoadedEvent;
+import io.th0rgal.oraxen.utils.logs.Logs;
 import org.bukkit.GameMode;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.PlayerInventory;
@@ -15,6 +19,16 @@ public class FoodMechanicListener implements Listener {
         this.factory = factory;
     }
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    private void registerReplacements(OraxenItemsLoadedEvent event) {
+        for (String itemID : factory.getItems()) {
+            FoodMechanic foodMechanic = (FoodMechanic) factory.getMechanic(itemID);
+            ConfigurationSection replacementSection = foodMechanic.getSection().getConfigurationSection("replacement");
+            if (replacementSection == null) continue;
+            foodMechanic.registerReplacement(replacementSection);
+        }
+    }
+
     @EventHandler
     public void onEatingFood(PlayerItemConsumeEvent event) {
         Player player = event.getPlayer();
@@ -23,7 +37,7 @@ public class FoodMechanicListener implements Listener {
         if (itemID == null || factory.isNotImplementedIn(itemID)) return;
         FoodMechanic mechanic = (FoodMechanic) factory.getMechanic(itemID);
         event.setCancelled(true);
-
+        Logs.debug(mechanic.hasEffects());
         if (player.getGameMode() != GameMode.CREATIVE) {
             if (mechanic.hasReplacement())
                 inventory.setItemInMainHand(mechanic.getReplacement());
