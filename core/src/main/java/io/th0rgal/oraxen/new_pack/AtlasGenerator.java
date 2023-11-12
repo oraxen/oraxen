3acfe99c93b697985cb399095172feb9c81bf6dd
Boy <sivertpaulsen2@gmail.com>
2023-11-12 14:22:33 +0100

    fix: atlas including minecraft:item/ & minecraft:block/

--- core/src/main/java/io/th0rgal/oraxen/new_pack/AtlasGenerator.java
+++ core/src/main/java/io/th0rgal/oraxen/new_pack/AtlasGenerator.java
@@ -5,6 +5,7 @@ import io.th0rgal.oraxen.utils.logs.Logs;
 import net.kyori.adventure.key.Key;
 import team.unnamed.creative.atlas.Atlas;
 import team.unnamed.creative.atlas.AtlasSource;
+import team.unnamed.creative.atlas.SingleAtlasSource;
 import team.unnamed.creative.model.Model;
 import team.unnamed.creative.model.ModelTexture;
 import team.unnamed.creative.model.ModelTextures;
@@ -23,6 +24,13 @@ public class AtlasGenerator {
             sources.addAll(model.textures().variables().values().stream().map(t -> AtlasSource.single(t.key())).toList());
             if (model.textures().particle() != null) sources.add(AtlasSource.single(model.textures().particle().key()));
         }
+
+        // Remove everything in the item and block folders, as vanilla already has them
+        sources.stream().map(SingleAtlasSource.class::cast).filter(r -> {
+            String resource = r.resource().asString().replace("minecraft:", "");
+            return resource.startsWith("item/") || resource.startsWith("block/");
+        }).toList().forEach(sources::remove);
+
         OraxenPlugin.get().resourcePack().atlas(Atlas.atlas(Atlas.BLOCKS, sources));
     }
 }
