1ca3fdb36db182694edd15ae38ce45a379330f75
Boy <sivertpaulsen2@gmail.com>
2023-11-20 22:47:52 +0100

    chore: remove leftover Gesture code

--- core/src/main/java/io/th0rgal/oraxen/config/ConfigsManager.java
+++ core/src/main/java/io/th0rgal/oraxen/config/ConfigsManager.java
@@ -40,7 +40,6 @@ public class ConfigsManager {
     private File itemsFolder;
     private File glyphsFolder;
     private File schematicsFolder;
-    private File gesturesFolder;
 
     public ConfigsManager(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -134,15 +133,6 @@ public class ConfigsManager {
             if (Settings.GENERATE_DEFAULT_CONFIGS.toBool())
                 new ResourcesManager(plugin).extractConfigsInFolder("schematics", "schem");
         }
-
-        // check gestures
-        gesturesFolder = new File(plugin.getDataFolder(), "gestures");
-        if (!gesturesFolder.exists()) {
-            gesturesFolder.mkdirs();
-            if (Settings.GENERATE_DEFAULT_CONFIGS.toBool())
-                new ResourcesManager(plugin).extractConfigsInFolder("gestures", "yml");
-        }
-
     }
 
     private YamlConfiguration validate(ResourcesManager resourcesManager, String configName, YamlConfiguration defaultConfiguration) {
