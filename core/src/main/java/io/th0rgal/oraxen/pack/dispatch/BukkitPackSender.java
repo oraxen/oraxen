6b34ba7ff887054e5f5ca7a47e1af41f0504ce13
CubeCrafter <86047353+CubeCrafter72@users.noreply.github.com>
2023-12-26 16:06:23 +0100

    fix resource pack being sent multiple times (#1167)

--- core/src/main/java/io/th0rgal/oraxen/pack/dispatch/BukkitPackSender.java
+++ core/src/main/java/io/th0rgal/oraxen/pack/dispatch/BukkitPackSender.java
@@ -35,8 +35,9 @@ public class BukkitPackSender extends PackSender implements Listener {
     @Override
     public void sendPack(Player player) {
         if (VersionUtil.atOrAbove("1.20.3"))
-            player.setResourcePack(UUID.randomUUID(), hostingProvider.getMinecraftPackURL(), hostingProvider.getSHA1(), AdventureUtils.parseLegacy(prompt), mandatory);
-        if (VersionUtil.isPaperServer()) player.setResourcePack(hostingProvider.getMinecraftPackURL(), hostingProvider.getSHA1(), AdventureUtils.MINI_MESSAGE.deserialize(prompt), mandatory);
+            if (VersionUtil.isPaperServer()) player.setResourcePack(UUID.randomUUID(), hostingProvider.getMinecraftPackURL(), hostingProvider.getSHA1(), AdventureUtils.MINI_MESSAGE.deserialize(prompt), mandatory);
+            else player.setResourcePack(UUID.randomUUID(), hostingProvider.getMinecraftPackURL(), hostingProvider.getSHA1(), AdventureUtils.parseLegacy(prompt), mandatory);
+        else if (VersionUtil.isPaperServer()) player.setResourcePack(hostingProvider.getMinecraftPackURL(), hostingProvider.getSHA1(), AdventureUtils.MINI_MESSAGE.deserialize(prompt), mandatory);
         else player.setResourcePack(hostingProvider.getMinecraftPackURL(), hostingProvider.getSHA1(), AdventureUtils.parseLegacy(prompt), mandatory);
     }
 
