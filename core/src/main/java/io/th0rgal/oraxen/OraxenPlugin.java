fdb8b6f7d34ae2661bececad93aa1e67a74193c8
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2024-03-18 03:58:29 +0100

    Modify deprecated method

--- core/src/main/java/io/th0rgal/oraxen/OraxenPlugin.java
+++ core/src/main/java/io/th0rgal/oraxen/OraxenPlugin.java
@@ -41,6 +41,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
@@ -72,12 +73,8 @@ public class OraxenPlugin extends JavaPlugin {
         return oraxen;
     }
 
-    public static Energie getEnergieTask() {
-        return energie;
-    }
-
-    public static Scheduler getScheduler() {
-        return energie.getScheduler(Energie.SchedulerSoft.MINECRAFT);
+    public @NotNull static Scheduler getScheduler() {
+        return energie.getMinecraftScheduler();
     }
 
 
