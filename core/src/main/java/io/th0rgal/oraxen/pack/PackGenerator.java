dcddf9fc6288b4eb64e08b89fe189c83d6d472b3
Boy <sivertpaulsen2@gmail.com>
2024-03-23 13:35:16 +0100

    fix: parsing of Language files

--- core/src/main/java/io/th0rgal/oraxen/pack/PackGenerator.java
+++ core/src/main/java/io/th0rgal/oraxen/pack/PackGenerator.java
@@ -5,6 +5,7 @@ import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.config.Settings;
 import io.th0rgal.oraxen.font.FontManager;
 import io.th0rgal.oraxen.font.Glyph;
+import io.th0rgal.oraxen.utils.AdventureUtils;
 import io.th0rgal.oraxen.utils.ModelEngineUtils;
 import io.th0rgal.oraxen.utils.VersionUtil;
 import io.th0rgal.oraxen.utils.customarmor.CustomArmor;
@@ -29,6 +30,7 @@ import team.unnamed.creative.sound.SoundRegistry;
 import java.io.File;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PackGenerator {
 
@@ -55,6 +57,7 @@ public class PackGenerator {
 
         addItemPackFiles();
         addGlyphFiles();
+        parseLanguageFiles();
         customArmorHandler.generateNeededFiles();
         if (Settings.HIDE_SCOREBOARD_NUMBERS.toBool()) hideScoreboardNumbers();
         if (Settings.HIDE_SCOREBOARD_BACKGROUND.toBool()) hideScoreboardBackground();
@@ -70,6 +73,19 @@ public class PackGenerator {
 
         builtPack = MinecraftResourcePackWriter.minecraft().build(resourcePack);
         Logs.logSuccess("Finished generating resourcepack!", true);
+    }
+
+    public ResourcePack resourcePack() {
+        if (resourcePack == null) resourcePack = ResourcePack.resourcePack();
+        return resourcePack;
+    }
+
+    public void resourcePack(ResourcePack resourcePack) {
+        this.resourcePack = resourcePack;
+    }
+
+    public BuiltResourcePack builtPack() {
+        return builtPack;
     }
 
     private void importModelEnginePack() {
@@ -161,7 +177,7 @@ public class PackGenerator {
 
         importedPack.atlases().forEach(atlas -> {
             Atlas baseAtlas = resourcePack.atlas(atlas.key());
-            if (baseAtlas != null) atlas.sources().forEach( source -> baseAtlas.toBuilder().addSource(source));
+            if (baseAtlas != null) atlas.sources().forEach(source -> baseAtlas.toBuilder().addSource(source));
             resourcePack.atlas(atlas);
         });
 
@@ -170,18 +186,6 @@ public class PackGenerator {
             if (baseLanguage != null) baseLanguage.translations().putAll(language.translations());
             resourcePack.language(language);
         });
-    }
-
-    public ResourcePack resourcePack() {
-        if (resourcePack == null) resourcePack = ResourcePack.resourcePack();
-        return resourcePack;
-    }
-    public void resourcePack(ResourcePack resourcePack) {
-        this.resourcePack = resourcePack;
-    }
-
-    public BuiltResourcePack builtPack() {
-        return builtPack;
     }
 
     private static void generateDefaultPaths() {
@@ -199,7 +203,55 @@ public class PackGenerator {
         AtlasGenerator.generateAtlasFile();
     }
 
+    private void parseLanguageFiles() {
+        parseGlobalLanguage();
+        for (Language language : new ArrayList<>(resourcePack.languages())) {
+            for (Map.Entry<String, String> entry : new HashSet<>(language.translations().entrySet())) {
+                language.translations().put(entry.getKey(), AdventureUtils.parseLegacyThroughMiniMessage(entry.getValue()));
+                language.translations().remove("DO_NOT_ALTER_THIS_LINE");
+            }
+            resourcePack.language(language);
+        }
+    }
+
+    private void parseGlobalLanguage() {
+        Language globalLanguage = resourcePack.language(Key.key("global"));
+        if (globalLanguage == null) return;
+        Logs.logInfo("Converting global lang file to individual language files...");
+
+        for (Key langKey : availableLanguageCodes) {
+            Language language = resourcePack.language(langKey);
+            if (language == null) language = Language.language(langKey, Map.of());
+            Map<String, String> newTranslations = new HashMap<>(language.translations());
+
+            for (Map.Entry<String, String> globalEntry : new HashSet<>(globalLanguage.translations().entrySet())) {
+                newTranslations.putIfAbsent(globalEntry.getKey(), globalEntry.getValue());
+            }
+            resourcePack.language(Language.language(langKey, newTranslations));
+        }
+        resourcePack.removeLanguage(Key.key("global"));
+    }
+
+    private static final Set<Key> availableLanguageCodes = new HashSet<>(Arrays.asList(
+            "af_za", "ar_sa", "ast_es", "az_az", "ba_ru",
+            "bar", "be_by", "bg_bg", "br_fr", "brb", "bs_ba", "ca_es", "cs_cz",
+            "cy_gb", "da_dk", "de_at", "de_ch", "de_de", "el_gr", "en_au", "en_ca",
+            "en_gb", "en_nz", "en_pt", "en_ud", "en_us", "enp", "enws", "eo_uy",
+            "es_ar", "es_cl", "es_ec", "es_es", "es_mx", "es_uy", "es_ve", "esan",
+            "et_ee", "eu_es", "fa_ir", "fi_fi", "fil_ph", "fo_fo", "fr_ca", "fr_fr",
+            "fra_de", "fur_it", "fy_nl", "ga_ie", "gd_gb", "gl_es", "haw_us", "he_il",
+            "hi_in", "hr_hr", "hu_hu", "hy_am", "id_id", "ig_ng", "io_en", "is_is",
+            "isv", "it_it", "ja_jp", "jbo_en", "ka_ge", "kk_kz", "kn_in", "ko_kr",
+            "ksh", "kw_gb", "la_la", "lb_lu", "li_li", "lmo", "lol_us", "lt_lt",
+            "lv_lv", "lzh", "mk_mk", "mn_mn", "ms_my", "mt_mt", "nah", "nds_de",
+            "nl_be", "nl_nl", "nn_no", "no_no", "oc_fr", "ovd", "pl_pl", "pt_br",
+            "pt_pt", "qya_aa", "ro_ro", "rpr", "ru_ru", "ry_ua", "se_no", "sk_sk",
+            "sl_si", "so_so", "sq_al", "sr_sp", "sv_se", "sxu", "szl", "ta_in",
+            "th_th", "tl_ph", "tlh_aa", "tok", "tr_tr", "tt_ru", "uk_ua", "val_es",
+            "vec_it", "vi_vn", "yi_de", "yo_ng", "zh_cn", "zh_hk", "zh_tw", "zlm_arab")).stream().map(Key::key).collect(Collectors.toSet());
+
     private final static Set<String> ignoredExtensions = new HashSet<>(Arrays.asList(".json", ".png", ".mcmeta"));
+
     private void removeExcludedFileExtensions() {
         for (String extension : Settings.EXCLUDED_FILE_EXTENSIONS.toStringList()) {
             extension = extension.startsWith(".") ? extension : "." + extension;
