30b4fa39264d4b603bf6b0235cd4dbe81cd2787f
Boy <sivertpaulsen2@gmail.com>
2024-03-24 23:28:56 +0100

    chore: remove more minor gesture leftover code

--- core/src/main/java/io/th0rgal/oraxen/config/ConfigsManager.java
+++ core/src/main/java/io/th0rgal/oraxen/config/ConfigsManager.java
@@ -40,7 +40,6 @@ public class ConfigsManager {
     private File itemsFolder;
     private File glyphsFolder;
     private File schematicsFolder;
-    private File gesturesFolder;
 
     public ConfigsManager(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -133,14 +132,6 @@ public class ConfigsManager {
             schematicsFolder.mkdirs();
             if (Settings.GENERATE_DEFAULT_CONFIGS.toBool())
                 tempManager.extractConfigsInFolder("schematics", "schem");
-        }
-
-        // check gestures
-        gesturesFolder = new File(plugin.getDataFolder(), "gestures");
-        if (!gesturesFolder.exists()) {
-            gesturesFolder.mkdirs();
-            if (Settings.GENERATE_DEFAULT_CONFIGS.toBool())
-                tempManager.extractConfigsInFolder("gestures", "yml");
         }
 
     }
