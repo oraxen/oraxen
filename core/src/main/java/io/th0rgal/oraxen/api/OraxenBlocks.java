d83af2a950abace21a1c49005c860c8d8d665458
Minusten <134551265+minustenchan@users.noreply.github.com>
2024-04-03 00:48:37 +0530

    fix: call World#playEffect only if server is 1.20 or above (#1326)

--- core/src/main/java/io/th0rgal/oraxen/api/OraxenBlocks.java
+++ core/src/main/java/io/th0rgal/oraxen/api/OraxenBlocks.java
@@ -275,7 +275,7 @@ public class OraxenBlocks {
             World world = block.getWorld();
 
             if (VersionUtil.isPaperServer()) world.sendGameEvent(player, GameEvent.BLOCK_DESTROY, loc.toVector());
-            world.playEffect(loc, Effect.STEP_SOUND, VersionUtil.atOrAbove("1.20") ? block.getBlockData() : null);
+            if (VersionUtil.atOrAbove("1.20")) world.playEffect(loc, Effect.STEP_SOUND, block.getBlockData());
         }
         if (drop != null) drop.spawns(loc, itemInHand);
 
