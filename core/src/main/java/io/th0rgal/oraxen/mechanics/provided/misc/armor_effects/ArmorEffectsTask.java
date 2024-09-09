9fca8a26b8663489a111fcafb310753dc4af0aa1
Euphyllia Bierque <bierque.euphyllia@gmail.com>
2024-09-10 00:12:25 +0200

    Fix Cannot add effects to entities asynchronously

--- core/src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/armor_effects/ArmorEffectsTask.java
+++ core/src/main/java/io/th0rgal/oraxen/mechanics/provided/misc/armor_effects/ArmorEffectsTask.java
@@ -1,16 +1,18 @@
 package io.th0rgal.oraxen.mechanics.provided.misc.armor_effects;
 
+import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.api.scheduler.AdaptedTaskRunnable;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class ArmorEffectsTask extends AdaptedTaskRunnable {
 
     @Override
     public void run() {
         for (Player player : Bukkit.getOnlinePlayers()) {
-            ArmorEffectsMechanic.addEffects(player);
+            OraxenPlugin.get().getScheduler().runEntityTask(player, () -> {
+                ArmorEffectsMechanic.addEffects(player);
+            }, null);
         }
     }
 }
