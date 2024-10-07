86381ccd7e055f430d53e8dc359988b209640a33
Boy <sivertpaulsen2@gmail.com>
2024-10-07 15:59:27 +0200

    refactor: minor code cleanup

--- v1_21_R1/src/main/java/io/th0rgal/oraxen/nms/v1_21_R1/GlyphHandler.java
+++ v1_21_R1/src/main/java/io/th0rgal/oraxen/nms/v1_21_R1/GlyphHandler.java
@@ -21,16 +21,19 @@ public class GlyphHandler implements io.th0rgal.oraxen.nms.GlyphHandler {
                 channel.pipeline().addBefore("packet_handler", GlyphHandlers.GLYPH_HANDLER_KEY.asString(), new ChannelDuplexHandler() {
                     @Override
                     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
-                        if (msg instanceof ClientboundSetTitleTextPacket packet && Settings.FORMAT_TITLES.toBool()) {
-                            msg = new ClientboundSetTitleTextPacket(PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.text()))));
-                        } else if (msg instanceof ClientboundSetSubtitleTextPacket packet && Settings.FORMAT_SUBTITLES.toBool()) {
-                            msg = new ClientboundSetSubtitleTextPacket(PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.text()))));
-                        } else if (msg instanceof ClientboundSetActionBarTextPacket packet && Settings.FORMAT_ACTION_BAR.toBool()) {
-                            msg = new ClientboundSetActionBarTextPacket(PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.text()))));
-                        } else if (msg instanceof ClientboundOpenScreenPacket packet && Settings.FORMAT_INVENTORY_TITLES.toBool()) {
-                            msg = new ClientboundOpenScreenPacket(packet.getContainerId(), packet.getType(), PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.getTitle()))));
-                        } else if (msg instanceof ClientboundSetScorePacket packet) {
-                            msg = new ClientboundSetScorePacket(packet.owner(), packet.objectiveName(), packet.score(), Settings.FORMAT_SCOREBOARD.toBool() ? packet.display().map(c -> PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(c)))) : packet.display(), Settings.HIDE_SCOREBOARD_NUMBERS.toBool() ? Optional.of(BlankFormat.INSTANCE) : packet.numberFormat());
+                        switch (msg) {
+                            case ClientboundSetTitleTextPacket packet when Settings.FORMAT_TITLES.toBool() ->
+                                    msg = new ClientboundSetTitleTextPacket(PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.text()))));
+                            case ClientboundSetSubtitleTextPacket packet when Settings.FORMAT_SUBTITLES.toBool() ->
+                                    msg = new ClientboundSetSubtitleTextPacket(PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.text()))));
+                            case ClientboundSetActionBarTextPacket packet when Settings.FORMAT_ACTION_BAR.toBool() ->
+                                    msg = new ClientboundSetActionBarTextPacket(PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.text()))));
+                            case ClientboundOpenScreenPacket packet when Settings.FORMAT_INVENTORY_TITLES.toBool() ->
+                                    msg = new ClientboundOpenScreenPacket(packet.getContainerId(), packet.getType(), PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(packet.getTitle()))));
+                            case ClientboundSetScorePacket packet ->
+                                    msg = new ClientboundSetScorePacket(packet.owner(), packet.objectiveName(), packet.score(), Settings.FORMAT_SCOREBOARD.toBool() ? packet.display().map(c -> PaperAdventure.asVanilla(GlyphHandlers.transformGlyphs(PaperAdventure.asAdventure(c)))) : packet.display(), Settings.HIDE_SCOREBOARD_NUMBERS.toBool() ? Optional.of(BlankFormat.INSTANCE) : packet.numberFormat());
+                            case null, default -> {
+                            }
                         }
 
                         ctx.write(msg, promise);
