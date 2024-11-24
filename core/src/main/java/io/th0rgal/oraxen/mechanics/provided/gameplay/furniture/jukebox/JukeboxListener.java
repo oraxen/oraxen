1aac09e07673fa3ef0f8d209a389f06996f2c29d
Thomas Marchand <thomas.marchand@tuta.io>
2024-11-24 13:00:17 +0800

    fix: jukebox option in noteblocks

--- core/src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/furniture/jukebox/JukeboxListener.java
+++ core/src/main/java/io/th0rgal/oraxen/mechanics/provided/gameplay/furniture/jukebox/JukeboxListener.java
@@ -12,6 +12,7 @@ import io.th0rgal.oraxen.utils.AdventureUtils;
 import io.th0rgal.oraxen.utils.BlockHelpers;
 import io.th0rgal.oraxen.utils.ItemUtils;
 import io.th0rgal.oraxen.utils.VersionUtil;
+import io.th0rgal.oraxen.utils.logs.Logs;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.text.Component;
@@ -40,10 +41,13 @@ public class JukeboxListener implements Listener {
         Player player = event.getPlayer();
         ItemStack itemStack = player.getInventory().getItemInMainHand();
 
-        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (event.getHand() != EquipmentSlot.HAND)
+            return;
 
         boolean played = insertAndPlayDisc(baseEntity, itemStack, player);
-        if (!played) return;
+
+        if (!played)
+            return;
         player.swingMainHand();
 
         String displayName = null;
@@ -58,7 +62,8 @@ public class JukeboxListener implements Listener {
         }
 
         if (displayName != null) {
-            Component message = AdventureUtils.MINI_MESSAGE.deserialize(Message.MECHANICS_JUKEBOX_NOW_PLAYING.toString(), AdventureUtils.tagResolver("disc", displayName));
+            Component message = AdventureUtils.MINI_MESSAGE.deserialize(
+                    Message.MECHANICS_JUKEBOX_NOW_PLAYING.toString(), AdventureUtils.tagResolver("disc", displayName));
             OraxenPlugin.get().getAudience().player(player).sendActionBar(message);
         }
 
@@ -67,7 +72,8 @@ public class JukeboxListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onEjectDisc(OraxenFurnitureInteractEvent event) {
-        if (!ejectAndStopDisc(event.getBaseEntity(), event.getPlayer())) return;
+        if (!ejectAndStopDisc(event.getBaseEntity(), event.getPlayer()))
+            return;
         event.getPlayer().swingMainHand();
         event.setCancelled(true);
     }
@@ -82,16 +88,31 @@ public class JukeboxListener implements Listener {
         FurnitureMechanic furnitureMechanic = OraxenFurniture.getFurnitureMechanic(baseEntity);
         Location loc = BlockHelpers.toCenterLocation(baseEntity.getLocation());
 
-        if (furnitureMechanic == null || !furnitureMechanic.isJukebox()) return false;
-        if (pdc.has(MUSIC_DISC_KEY, DataType.ITEM_STACK) || !ItemUtils.isMusicDisc(disc)) return false;
+        Logs.logWarning("alpha");
+        if (furnitureMechanic == null || !furnitureMechanic.isJukebox())
+            return false;
+
+        if (!ItemUtils.isMusicDisc(disc))
+            return false;
+
+        Logs.logWarning("beta");
+
+        if (pdc.has(MUSIC_DISC_KEY, DataType.ITEM_STACK))
+            return false;
+
+        Logs.logWarning("gamma");
         JukeboxBlock jukebox = furnitureMechanic.getJukebox();
-        if (!jukebox.hasPermission(player)) return false;
+        if (!jukebox.hasPermission(player))
+            return false;
+
+        Logs.logWarning("delta");
         ItemStack insertedDisc = disc.clone();
         insertedDisc.setAmount(1);
         if (player != null && player.getGameMode() != GameMode.CREATIVE)
             disc.setAmount(disc.getAmount() - insertedDisc.getAmount());
         pdc.set(MUSIC_DISC_KEY, DataType.ITEM_STACK, insertedDisc);
-        baseEntity.getWorld().playSound(loc, jukebox.getPlayingSong(baseEntity), SoundCategory.RECORDS, jukebox.getVolume(), jukebox.getPitch());
+        baseEntity.getWorld().playSound(loc, jukebox.getPlayingSong(baseEntity), SoundCategory.RECORDS,
+                jukebox.getVolume(), jukebox.getPitch());
         return true;
     }
 
@@ -101,19 +122,24 @@ public class JukeboxListener implements Listener {
         FurnitureMechanic furnitureMechanic = OraxenFurniture.getFurnitureMechanic(baseEntity);
         Location loc = BlockHelpers.toCenterLocation(baseEntity.getLocation());
 
-        if (furnitureMechanic == null || !furnitureMechanic.isJukebox()) return false;
-        if (!pdc.has(MUSIC_DISC_KEY, DataType.ITEM_STACK) || !ItemUtils.isMusicDisc(item)) return false;
+        if (furnitureMechanic == null || !furnitureMechanic.isJukebox())
+            return false;
+        if (!pdc.has(MUSIC_DISC_KEY, DataType.ITEM_STACK) || !ItemUtils.isMusicDisc(item))
+            return false;
 
         JukeboxBlock jukebox = furnitureMechanic.getJukebox();
-        if (!jukebox.hasPermission(player)) return false;
+        if (!jukebox.hasPermission(player))
+            return false;
 
         baseEntity.getWorld().getNearbyEntities(loc, 32, 32, 32).stream()
                 .filter(entity -> entity instanceof Player)
                 .map(entity -> (Player) entity)
                 .forEach(p -> {
                     Key songKey = getSongFromDisc(item);
-                    if (songKey == null) return;
-                    OraxenPlugin.get().getAudience().player(p).stopSound(Sound.sound(songKey, Sound.Source.RECORD, jukebox.getVolume(), jukebox.getPitch()));
+                    if (songKey == null)
+                        return;
+                    OraxenPlugin.get().getAudience().player(p).stopSound(
+                            Sound.sound(songKey, Sound.Source.RECORD, jukebox.getVolume(), jukebox.getPitch()));
                 });
         baseEntity.getWorld().dropItemNaturally(loc, item);
         pdc.remove(MUSIC_DISC_KEY);
@@ -122,12 +148,11 @@ public class JukeboxListener implements Listener {
 
     @Nullable
     private Key getSongFromDisc(ItemStack disc) {
-        if (VersionUtil.atOrAbove("1.21")) {
-            return disc.hasItemMeta() && disc.getItemMeta().hasJukeboxPlayable()
-                    ? disc.getItemMeta().getJukeboxPlayable().getSongKey().key()
-                    : null;
+        if (VersionUtil.atOrAbove("1.21") && disc.hasItemMeta() && disc.getItemMeta().hasJukeboxPlayable()) {
+            return disc.getItemMeta().getJukeboxPlayable().getSongKey().key();
         } else {
-            return Key.key("minecraft", "music_disc." + disc.getType().toString().toLowerCase(Locale.ROOT).split("music_disc_")[1]);
+            return Key.key("minecraft",
+                    "music_disc." + disc.getType().toString().toLowerCase(Locale.ROOT).split("music_disc_")[1]);
         }
     }
 }
--- core/src/main/java/io/th0rgal/oraxen/utils/ItemUtils.java
+++ core/src/main/java/io/th0rgal/oraxen/utils/ItemUtils.java
@@ -1,6 +1,8 @@
 package io.th0rgal.oraxen.utils;
 
 import io.th0rgal.oraxen.utils.drops.Drop;
+import io.th0rgal.oraxen.utils.logs.Logs;
+
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.Tag;
@@ -43,7 +45,8 @@ public class ItemUtils {
      */
     public static void editItemMeta(ItemStack itemStack, Consumer<ItemMeta> function) {
         ItemMeta meta = itemStack.getItemMeta();
-        if (meta == null) return;
+        if (meta == null)
+            return;
         function.accept(meta);
         itemStack.setItemMeta(meta);
     }
@@ -60,22 +63,27 @@ public class ItemUtils {
     public static void damageItem(Player player, Drop drop, ItemStack itemStack) {
 
         // If all are null this is not something Oraxen should handle
-        // If the block/furniture has no drop, it returns Drop.emptyDrop() which is handled by the caller
-        if (drop == null) return;
+        // If the block/furniture has no drop, it returns Drop.emptyDrop() which is
+        // handled by the caller
+        if (drop == null)
+            return;
 
         int damage;
         boolean isToolEnough = drop.isToolEnough(itemStack);
         damage = isToolEnough ? 1 : 2;
-        // If the item is not a tool, it will not be damaged, example flint&steel should not be damaged
+        // If the item is not a tool, it will not be damaged, example flint&steel should
+        // not be damaged
         damage = isTool(itemStack) ? damage : 0;
 
-        if (damage == 0) return;
+        if (damage == 0)
+            return;
         if (VersionUtil.isPaperServer() && VersionUtil.atOrAbove("1.19"))
             player.damageItemStack(itemStack, damage);
         else {
             int finalDamage = damage;
             editItemMeta(itemStack, meta -> {
-                if (meta instanceof Damageable damageable && EventUtils.callEvent(new PlayerItemDamageEvent(player, itemStack, finalDamage))) {
+                if (meta instanceof Damageable damageable
+                        && EventUtils.callEvent(new PlayerItemDamageEvent(player, itemStack, finalDamage))) {
                     damageable.setDamage(damageable.getDamage() + 1);
                 }
             });
@@ -89,40 +97,50 @@ public class ItemUtils {
     public static boolean isTool(@NotNull Material material) {
         if (VersionUtil.atOrAbove("1.19.4") && !VersionUtil.atOrAbove("1.20.5"))
             return Tag.ITEMS_TOOLS.isTagged(material);
-        else return material.toString().endsWith("_AXE")
-                || material.toString().endsWith("_PICKAXE")
-                || material.toString().endsWith("_SHOVEL")
-                || material.toString().endsWith("_HOE")
-                || material.toString().endsWith("_SWORD")
-                || material == Material.TRIDENT;
+        else
+            return material.toString().endsWith("_AXE")
+                    || material.toString().endsWith("_PICKAXE")
+                    || material.toString().endsWith("_SHOVEL")
+                    || material.toString().endsWith("_HOE")
+                    || material.toString().endsWith("_SWORD")
+                    || material == Material.TRIDENT;
     }
 
     public static boolean isSkull(Material material) {
         return switch (material) {
-            case PLAYER_HEAD, PLAYER_WALL_HEAD, SKELETON_SKULL, SKELETON_WALL_SKULL, WITHER_SKELETON_SKULL, WITHER_SKELETON_WALL_SKULL, ZOMBIE_HEAD, ZOMBIE_WALL_HEAD, CREEPER_HEAD, CREEPER_WALL_HEAD, DRAGON_HEAD, DRAGON_WALL_HEAD, PIGLIN_HEAD, PIGLIN_WALL_HEAD ->
-                    true;
+            case PLAYER_HEAD, PLAYER_WALL_HEAD, SKELETON_SKULL, SKELETON_WALL_SKULL, WITHER_SKELETON_SKULL,
+                    WITHER_SKELETON_WALL_SKULL, ZOMBIE_HEAD, ZOMBIE_WALL_HEAD, CREEPER_HEAD, CREEPER_WALL_HEAD,
+                    DRAGON_HEAD, DRAGON_WALL_HEAD, PIGLIN_HEAD, PIGLIN_WALL_HEAD ->
+                true;
             default -> false;
         };
     }
 
     public static boolean hasInventoryParent(Material material) {
-        return Tag.WALLS.isTagged(material) || Tag.FENCES.isTagged(material) || Tag.BUTTONS.isTagged(material) || material == Material.PISTON || material == Material.STICKY_PISTON || (VersionUtil.atOrAbove("1.20") && material == Material.CHISELED_BOOKSHELF) || material == Material.BROWN_MUSHROOM_BLOCK || material == Material.RED_MUSHROOM_BLOCK || material == Material.MUSHROOM_STEM;
+        return Tag.WALLS.isTagged(material) || Tag.FENCES.isTagged(material) || Tag.BUTTONS.isTagged(material)
+                || material == Material.PISTON || material == Material.STICKY_PISTON
+                || (VersionUtil.atOrAbove("1.20") && material == Material.CHISELED_BOOKSHELF)
+                || material == Material.BROWN_MUSHROOM_BLOCK || material == Material.RED_MUSHROOM_BLOCK
+                || material == Material.MUSHROOM_STEM;
     }
 
     public static boolean isMusicDisc(ItemStack itemStack) {
-        if (itemStack == null) return false;
-        if (VersionUtil.atOrAbove("1.21")) {
-            return itemStack.hasItemMeta() && itemStack.getItemMeta().hasJukeboxPlayable();
-        } else {
-            return itemStack.getType().name().startsWith("MUSIC_DISC");
+        if (itemStack == null)
+            return false;
+        // native disks don't seem to have jukebox playable set to true
+        if (VersionUtil.atOrAbove("1.21") && itemStack.hasItemMeta() && itemStack.getItemMeta().hasJukeboxPlayable()) {
+            return true;
         }
+        return itemStack.getType().name().startsWith("MUSIC_DISC");
     }
 
     @Nullable
     public static ItemStack getUsingConvertsTo(ItemMeta itemMeta) {
-        if (!VersionUtil.atOrAbove("1.21") || itemMeta == null) return null;
+        if (!VersionUtil.atOrAbove("1.21") || itemMeta == null)
+            return null;
 
-        if (VersionUtil.atOrAbove("1.21.2")) return itemMeta.hasUseRemainder() ? itemMeta.getUseRemainder() : null;
+        if (VersionUtil.atOrAbove("1.21.2"))
+            return itemMeta.hasUseRemainder() ? itemMeta.getUseRemainder() : null;
         try {
             return (ItemStack) FoodComponent.class.getMethod("getUsingConvertsTo").invoke(itemMeta.getFood());
         } catch (Exception e) {
