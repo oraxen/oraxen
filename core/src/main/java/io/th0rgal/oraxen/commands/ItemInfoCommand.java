ccc90e2261455ac49c8825b7ed136ec2938fe0e0
Thomas Marchand <thomas.marchand@tuta.io>
2024-12-15 19:44:02 +0500

    feat: show item_model in iteminfo command

--- core/src/main/java/io/th0rgal/oraxen/commands/ItemInfoCommand.java
+++ core/src/main/java/io/th0rgal/oraxen/commands/ItemInfoCommand.java
@@ -6,18 +6,23 @@ import dev.jorel.commandapi.arguments.StringArgument;
 import io.th0rgal.oraxen.OraxenPlugin;
 import io.th0rgal.oraxen.api.OraxenItems;
 import io.th0rgal.oraxen.items.ItemBuilder;
+import io.th0rgal.oraxen.items.OraxenMeta;
 import io.th0rgal.oraxen.utils.AdventureUtils;
 import io.th0rgal.oraxen.utils.logs.Logs;
 import net.kyori.adventure.audience.Audience;
 
 import java.util.Map;
 
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public class ItemInfoCommand {
 
     CommandAPICommand getItemInfoCommand() {
         return new CommandAPICommand("iteminfo")
                 .withPermission("oraxen.command.iteminfo")
-                .withArguments(new StringArgument("itemid").replaceSuggestions(ArgumentSuggestions.strings(OraxenItems.getItemNames())))
+                .withArguments(new StringArgument("itemid")
+                        .replaceSuggestions(ArgumentSuggestions.strings(OraxenItems.getItemNames())))
                 .executes((commandSender, args) -> {
                     String argument = (String) args.get("itemid");
                     Audience audience = OraxenPlugin.get().getAudience().sender(commandSender);
@@ -28,17 +33,50 @@ public class ItemInfoCommand {
                     } else {
                         ItemBuilder ib = OraxenItems.getItemById(argument);
                         if (ib == null)
-                            audience.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<red>No item found with ID</red> <dark_red>" + argument));
-                        else sendItemInfo(audience, ib, argument);
+                            audience.sendMessage(AdventureUtils.MINI_MESSAGE
+                                    .deserialize("<red>No item found with ID</red> <dark_red>" + argument));
+                        else
+                            sendItemInfo(audience, ib, argument);
                     }
                 });
     }
 
     private void sendItemInfo(Audience sender, ItemBuilder builder, String itemId) {
+        ItemStack item = builder.build();
+        ItemMeta meta = item.getItemMeta();
+
         sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_aqua>ItemID: <aqua>" + itemId));
-        sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_green>CustomModelData: <green>" + builder.getOraxenMeta().getCustomModelData()));
-        sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_green>Material: <green>" + builder.getReferenceClone().getType()));
-        sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_green>Model Name: <green>" + builder.getOraxenMeta().getModelName()));
+        sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_green>Material: <green>" + item.getType()));
+
+        // Basic meta info
+        if (meta != null) {
+            sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_green>==== Item Meta ===="));
+            if (meta.hasCustomModelData()) {
+                sender.sendMessage(AdventureUtils.MINI_MESSAGE
+                        .deserialize("<dark_green>CustomModelData: <green>" + meta.getCustomModelData()));
+            }
+            if (meta.hasItemModel()) {
+                sender.sendMessage(AdventureUtils.MINI_MESSAGE
+                        .deserialize("<dark_green>ItemModel: <green>" + meta.getItemModel()));
+            }
+            if (meta.hasDisplayName()) {
+                sender.sendMessage(AdventureUtils.MINI_MESSAGE
+                        .deserialize("<dark_green>DisplayName: <green>" + meta.getDisplayName()));
+            }
+        }
+
+        // OraxenMeta info
+        OraxenMeta oraxenMeta = builder.getOraxenMeta();
+        if (oraxenMeta != null) {
+            sender.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<dark_green>==== Oraxen Meta ===="));
+            sender.sendMessage(AdventureUtils.MINI_MESSAGE
+                    .deserialize("<dark_green>ModelName: <green>" + oraxenMeta.getModelName()));
+            if (oraxenMeta.hasPackInfos()) {
+                sender.sendMessage(AdventureUtils.MINI_MESSAGE
+                        .deserialize("<dark_green>ModelPath: <green>" + oraxenMeta.getGeneratedModelPath()));
+            }
+        }
+
         Logs.newline();
     }
 }
