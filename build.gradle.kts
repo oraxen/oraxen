34b4a7fd2195c813d423d7d64f0df4a2b4ff6c1c
Thomas Marchand <thomas.marchand@tuta.io>
2025-07-14 10:11:32 +0100

    update: preliminary 1.21.7 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     //id("com.github.johnrengelman.shadow") version "8.1.1"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0" // Generates plugin.yml
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.11" apply false
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17" apply false
     id("io.github.goooler.shadow") version "8.1.8"
 }
 
@@ -23,7 +23,8 @@ val SUPPORTED_VERSIONS: List<NMSVersion> = listOf(
     "v1_21_R1" toNms "1.21.1-R0.1-SNAPSHOT",
     "v1_21_R2" toNms "1.21.3-R0.1-SNAPSHOT",
     "v1_21_R3" toNms "1.21.4-R0.1-SNAPSHOT",
-    "v1_21_R4" toNms "1.21.5-R0.1-SNAPSHOT"
+    "v1_21_R4" toNms "1.21.5-R0.1-SNAPSHOT",
+    "v1_21_R5" toNms "1.21.7-R0.1-SNAPSHOT"
 )
 
 val compiled = (project.findProperty("oraxen_compiled")?.toString() ?: "true").toBoolean()
--- v1_21_R3/build.gradle.kts
+++ v1_21_R3/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.11"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
     id("io.github.goooler.shadow") version "8.1.8"
 }
 
