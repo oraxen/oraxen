c4a32000ec1c5c8a799332f6dd277d75b4a0d85c
Thomas Marchand <thomas.marchand@tuta.io>
2025-07-14 10:33:34 +0100

    fix: command API version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ val devPluginPath = project.findProperty("oraxen_dev_plugin_path")?.toString()
 val foliaPluginPath = project.findProperty("oraxen_folia_plugin_path")?.toString()
 val spigotPluginPath = project.findProperty("oraxen_spigot_plugin_path")?.toString()
 val pluginVersion: String by project
-val commandApiVersion = "10.0.0"
+val commandApiVersion = "10.1.1"
 val adventureVersion = "4.17.0"
 val platformVersion = "4.3.4"
 val googleGsonVersion = "2.10.1"
