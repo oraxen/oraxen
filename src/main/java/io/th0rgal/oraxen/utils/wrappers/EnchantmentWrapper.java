ea929249151ed7f90cd25ee1f17c211e57320968
fenjastical <298900722+fenjastical@users.noreply.github.com>
2026-07-02 01:58:38 +0200

    Restore enchantment version guard

--- src/main/java/io/th0rgal/oraxen/utils/wrappers/EnchantmentWrapper.java
+++ src/main/java/io/th0rgal/oraxen/utils/wrappers/EnchantmentWrapper.java
@@ -1,7 +1,6 @@
 package io.th0rgal.oraxen.utils.wrappers;
 
-import io.papermc.paper.registry.RegistryAccess;
-import io.papermc.paper.registry.RegistryKey;
+import io.th0rgal.oraxen.utils.VersionUtil;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Registry;
 import org.bukkit.enchantments.Enchantment;
@@ -11,14 +10,11 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Objects;
 
 public class EnchantmentWrapper {
-    private static final Registry<Enchantment> ENCHANTMENTS = RegistryAccess.registryAccess()
-            .getRegistry(RegistryKey.ENCHANTMENT);
-
     public static final @NotNull Enchantment FORTUNE = Objects.requireNonNull(getByKey(NamespacedKey.minecraft("fortune")));
     public static final @NotNull Enchantment EFFICIENCY = Objects.requireNonNull(getByKey(NamespacedKey.minecraft("efficiency")));
     public static final @NotNull Enchantment SILK_TOUCH = Objects.requireNonNull(getByKey(NamespacedKey.minecraft("silk_touch")));
 
     public static @Nullable Enchantment getByKey(@NotNull NamespacedKey key) {
-        return ENCHANTMENTS.get(key);
+        return VersionUtil.atOrAbove("1.21") ? Registry.ENCHANTMENT.get(key) : Enchantment.getByKey(key);
     }
 }
